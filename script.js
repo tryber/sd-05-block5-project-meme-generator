f610a617cb497161aaf4ab55e4d641bc92aa9fb8
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-05-22 12:50:24 -0300

    Inserction of the text typed on the box inside the correspondent div

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//Creation of the element related to the text input and input of its value inside of the correspondent div
 let textInput = document.getElementById("text-input");
+let memeText = "";
 textInput.addEventListener('keyup', getText);
 function getText(){
-  var memeText = textInput.nodeValue;
-  return memeText;
+  memeText = textInput.value;
+  document.querySelector("#meme-text").innerHTML = memeText;
 }
 
