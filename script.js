3c3c7893ea6f40f0472b5cb54db364214361c5e9
pedrohcalado <pedrocalado22@gmail.com>
2020-05-22 15:29:39 -0300

    Create input tag and container

---
+++ script.js
@@ -0,0 +1,5 @@
+//create variable to store text input
+let txt = document.querySelector("#text-input");
+
+//add the text input to the div meme-text
+document.querySelector("#meme-text").innerHTML = txt.value;
