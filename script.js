4019f924fb8f35aadf76a147b6837558e196080a
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-05-22 19:47:39 -0300

    Adicionando o evento do meme-input.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const memeInput = document.getElementById("meme-input");
+const memeInput = document.getElementById("meme-insert");
 const memeImage = document.getElementById("meme-image");
-
 memeInput.addEventListener("input", function(){
   memeImage.src = URL.createObjectURL(this.files[0]);
 });
