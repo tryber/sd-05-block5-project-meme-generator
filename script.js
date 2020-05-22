c38748e6bf0afac5997f6bb5b59975bfbd19f1b0
Felipe Vieira <flpvieira86@gmail.com>
2020-05-22 16:41:34 -0300

    adicionada sombra e definido o tamanho da letra do texto na imagem

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const textInput = document.getElementById('text-input');
 const caixaTxtDoMeme = document.getElementById('meme-text');
 const imgInput = document.getElementById('meme-insert');
 
-// eventListeners
-textInput.addEventListener('keyup', criaTxtDoMeme);
-imgInput.addEventListener('input', function () {
-  memeImage.src = URL.createObjectURL(this.files[0]);
-});
 // Functions
 
 function criaTxtDoMeme() {
   caixaTxtDoMeme.innerText = textInput.value;
 }
+
+// eventListeners
+textInput.addEventListener('keyup', criaTxtDoMeme);
+imgInput.addEventListener('input', function () {
+  memeImage.src = URL.createObjectURL(this.files[0]);
+});
