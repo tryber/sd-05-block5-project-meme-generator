209dd7e60490886364c0468420e02fe4352476c4
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-05-22 18:33:54 -0300

    Code climate 4

--- script.js
+++ script.js
@@ -41,29 +41,29 @@ buttonEarth.addEventListener('click', changeColor);
 
 // Load memes the are already done
 const loadMeme1 = document.getElementById('meme-1').firstChild;
-loadMeme1.addEventListener('click', loadMemeDone1);
 function loadMemeDone1() {
   document.getElementById('meme-image').src = loadMeme1.src;
   document.getElementById('meme-text').innerHTML = 'É o que que está acontencendo?';
 }
+loadMeme1.addEventListener('click', loadMemeDone1);
 
+const loadMeme2 = document.getElementById('meme-2').firstChild;
 function loadMemeDone2() {
   document.getElementById('meme-image').src = loadMeme2.src;
   document.getElementById('meme-text').innerHTML = 'Ehhhh... Melhor não';
 }
-const loadMeme2 = document.getElementById('meme-2').firstChild;
 loadMeme2.addEventListener('click', loadMemeDone2);
 
+const loadMeme3 = document.getElementById('meme-3').firstChild;
 function loadMemeDone3() {
   document.getElementById('meme-image').src = loadMeme3.src;
   document.getElementById('meme-text').innerHTML = 'Cri cri cri';
 }
-const loadMeme3 = document.getElementById('meme-3').firstChild;
 loadMeme3.addEventListener('click', loadMemeDone3);
 
+const loadMeme4 = document.getElementById('meme-4').firstChild;
 function loadMemeDone4() {
   document.getElementById('meme-image').src = loadMeme4.src;
   document.getElementById('meme-text').innerHTML = 'Não peguei a referencia';
 }
-const loadMeme4 = document.getElementById('meme-4').firstChild;
 loadMeme4.addEventListener('click', loadMemeDone4);
