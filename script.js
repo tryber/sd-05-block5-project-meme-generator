87ab7222c332892fdc9085d708a82ea47107b2ae
matambrosio <matmagamb1@gmail.com>
2020-05-22 16:10:39 -0300

    tentativa de atualizar texto

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-if
+
+let txtimput= document.querySelector("#imp");
+txtimput.addEventListener("keydown", function(){
+  let textMeme = document.querySelector("#meme-text");
+  console.log(textMeme.innerHTML.length)
+  textMeme.innerHTML = " "
+  if(txtimput.value.length < 60){
+    textMeme.innerHTML = txtimput.value;
+  }
+});
