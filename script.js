fea0a15d9effe40833233694642ca2fdc060f3a1
Rafael Assad <rafaassadeng@gmail.com>
2020-05-22 18:56:52 -0300

    funciona github

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = () => {
     //Variables
-    const memeImage = this.document.getElementById("meme-image"); //box image
-    const memeInsert = this.document.getElementById("meme-insert"); //select image
-    const memeText = this.document.getElementById("meme-text"); //text image
-    const textInput = this.document.getElementById("text-input"); //box text
+    const memeImage = document.getElementById("meme-image"); //box image
+    const memeInsert = document.getElementById("meme-insert"); //select image
+    const memeText = document.getElementById("meme-text"); //text image
+    const textInput = document.getElementById("text-input"); //box text
   
     //Event + Function uploadImage
     memeInsert.addEventListener("input", () => memeImage.src = URL.createObjectURL(memeInsert.files[0]));
@@ -11,7 +11,9 @@ window.onload = () => {
     //Event + Function textImage
     textInput.addEventListener("input", () => memeText.innerHTML = textInput.value);
   };
+
+
   
   function openImage(image) {
     document.getElementById("meme-image").src = image
-  }
+  };
