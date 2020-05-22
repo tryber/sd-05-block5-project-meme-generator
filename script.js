fdc12e4d15e59436e093a09a7d7f168cdb84fd93
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-05-22 19:31:59 -0300

    add 3 buttons that changes the style of the container

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ let outputImage = document.getElementById('meme-image');
 inputImage.addEventListener("input", function(){
   outputImage.src = URL.createObjectURL(this.files[0]);
 })
+
+let fire = document.getElementById('fire');
+let water = document.getElementById('water');
+let earth = document.getElementById('earth');
+let container = document.getElementById('meme-image-container');
+
+fire.addEventListener('click', function(){
+  container.style.border = '3px dashed red';
+})
+water.addEventListener('click', function(){
+  container.style.border = '5px double blue';
+})
+earth.addEventListener('click', function(){
+  container.style.border = '6px groove green';
+})
