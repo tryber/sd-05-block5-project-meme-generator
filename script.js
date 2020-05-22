f3d29d9fc4f5fa331e2b5b18c980a365661e6d31
Lucas Staroscky <staroscky60@gmail.com>
2020-05-22 19:26:58 -0300

    requisitos completos

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 window.onload = function () {
 
- let textInput = document.getElementById("text-input");
- let memeInsert = document.getElementById("meme-insert");
- let memeContainer = document.getElementById("meme-image-container");
- let memeText = document.getElementById("meme-text");
- let memeImage = document.getElementById("meme-image");
+ const textInput = document.getElementById("text-input");
+ const memeInsert = document.getElementById("meme-insert");
+ const memeText = document.getElementById("meme-text");
+ const memeImage = document.getElementById("meme-image");
 
- textInput.addEventListener("keyup", (event) => {
-  memeText.innerHTML = textInput.value;
- })
+ textInput.addEventListener("keyup", () => {
+   memeText.innerHTML = textInput.value;
+ });
 
  memeInsert.addEventListener("input", (event) => {
-  memeImage.src = URL.createObjectURL(event.target.files[0]);
- })
+   memeImage.src = URL.createObjectURL(event.target.files[0]);
+ });
 }
+
