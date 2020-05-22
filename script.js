8a731448318a91b62d628150d9f339e4af2ae6be
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-22 16:07:51 -0300

    created first function to get user text input

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
-var personName = "";
-function complainMessage() {
-  alert("I told you not to do that " + personName);
+// var personName = "";
+// function complainMessage() {
+//   alert("I told you not to do that " + personName);
+
+// let buttonDont = document.getElementsByTagName("button")[1];
+// buttonDont.addEventListener("click", complainMessage);
+
+let textInput = document.getElementById("text-input");
+let picInput = document.getElementById("meme-insert");
+
+let textMeme = document.getElementById("meme-text");
+let picMeme = document.getElementById("meme-image");
+
+let userTextMeme = "";
+function getTextInput () {
+  userTextMeme = textInput.value;
+  textMeme.innerHTML = userTextMeme;
 }
 
-function typeAndScream() {
-  document.getElementById("audio").play();
-  personName = document.getElementById("name").value;
-}
+textInput.addEventListener("keyup", getTextInput);
 
-function goDark() {
-  var body = document.getElementsByTagName("body")[0];
-  if (body.className == "") {
-    body.className = "dark";
-  }
-  else {
-    body.className = "";
-  }
 
-}
 
-function finishTheStory() {
-  document.getElementById("story").innerHTML = "So I carefully searched the damp dungeon. There you are! I grinned a smile with all 98 of my gleaming teeth. Dinner time!";
-}
 
-let buttonMood = document.getElementsByTagName("button")[0];
-buttonMood.addEventListener("click", goDark);
 
-let userInput = document.querySelector(".textEntry");
-userInput.addEventListener("input", typeAndScream);
-
-let buttonDont = document.getElementsByTagName("button")[1];
-buttonDont.addEventListener("click", complainMessage);
-
-let buttonStory = document.getElementsByTagName("button")[2];
-buttonStory.addEventListener("click", finishTheStory);
