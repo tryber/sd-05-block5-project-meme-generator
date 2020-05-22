e7a8f70a31f246042d600ae359b3914caae393ea
pedrohcalado <pedrocalado22@gmail.com>
2020-05-22 15:48:47 -0300

    Adjust script tag

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //create variable to store text input
-let txt = document.querySelector("#text-input");
+let txt = window.document.querySelector("#text-input");
 
 //add a event listener to add the text to the div meme-text when the user types it
 txt.addEventListener("keyup", function() {
   document.querySelector("#meme-text").innerHTML = txt.value;
 })
+
+console.log(txt);
