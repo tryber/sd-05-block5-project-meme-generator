490d8eca77cc8a8e26809c5d58f1971030164e94
Christian Duessel <duessel@gmail.com>
2020-05-22 18:36:53 -0300

    colocando fotinho

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 // let tinput = document.getElementById("text-input")
 
+// colocar texto na foto:
 function colocatextomeme() {
 let col = document.getElementById("text-input").value;
 document.getElementsByTagName("h2")[0].innerHTML = col;
@@ -9,3 +10,19 @@ document.getElementsByTagName("h2")[0].innerHTML = col;
 
 document.getElementById("text-input").addEventListener("keyup", colocatextomeme)
 
+// colocar nova foto
+
+document.getElementById("meme-insert").addEventListener("change", trocaFoto)
+
+function trocaFoto(event) {
+  var selectedFile = event.target.files[0];
+  var reader = new FileReader();
+
+  var imgtag = document.getElementById("meme-image");
+  
+  reader.onload = function(event) {
+    imgtag.src = event.target.result;
+  };
+
+  reader.readAsDataURL(selectedFile);
+}
