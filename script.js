a242c015ea934d0e91c8117e10a24a9abe6affde
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-05-22 15:28:56 -0300

    Inserçao da imagem na pagina

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-//Creation of the element related to the text input and input of its value inside of the correspondent div
-let textInput = document.getElementById("text-input");
+// Creation of the element related to the text input and input of its value inside of the correspondent div
+let textInput = document.getElementById('text-input');
 let memeText = "";
 textInput.addEventListener('keyup', getText);
-function getText(){
+function getText() {
   memeText = textInput.value;
   document.querySelector("#meme-text").innerHTML = memeText;
 }
 
-let inpputIMG = document.getElementById("img-input");
+/*let inpputIMG = document.getElementById("img-input");
 let memeIMG = "";
 inpputIMG.addEventListener('change', getImage);
-function getImage(element){
+function getImage(element) {
   memeIMG = element.target.result;
-  //memeIMG = inpputIMG.value;
+  // memeIMG = inpputIMG.value;
   console.log(memeIMG);
   document.getElementsByTagName("img")[0].src = memeIMG;
-  //document.querySelector("#meme-image-container").style.backgroundImage = "url(" + memeIMG + ")";
+  // document.querySelector("#meme-image-container").style.backgroundImage = "url(" + memeIMG + ")";
+}*/
+
+function readImage() {
+  if (this.files && this.files[0]) {
+      var file = new FileReader();
+      file.onload = function(e) {
+          document.getElementById("image").src = e.target.result;
+      };       
+      file.readAsDataURL(this.files[0]);
+  }
 }
+document.getElementById("img-input").addEventListener("change", readImage, false);
 
