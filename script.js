6bd67a2bc9b28b7385fe3713a77511dff5c92e59
pedrohcalado <pedrocalado22@gmail.com>
2020-05-22 16:56:08 -0300

    Put image inside the container

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-//create variable to store text input
-let txt = window.document.querySelector("#text-input");
+// create variable to store text input
+const txt = window.document.querySelector('#text-input');
 
-//add a event listener to add the text to the div meme-text when the user types it
-txt.addEventListener("keyup", function() {
-  document.querySelector("#meme-text").innerHTML = txt.value;
-})
+// add a event listener to add the text to the div meme-text when the user types it
+txt.addEventListener('keyup', function () {
+  document.querySelector('#meme-text').innerHTML = txt.value;
+});
 
-//load image
+// load image
 function loadFile(event) {
-    let output = document.getElementById('meme-image');
-    output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function() {
-      URL.revokeObjectURL(output.src) // free memory
-    }
-  };
+  const output = document.getElementById('meme-image');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function () {
+    URL.revokeObjectURL(output.src);
+  }
+}
