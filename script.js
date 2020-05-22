1bc7a9c4101a5ef720f144bbf9c3e0f437bf4979
matambrosio <matmagamb1@gmail.com>
2020-05-22 16:56:30 -0300

    tentativa 2 de atualizar texto

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 
 let txtimput= document.querySelector("#imp");
-txtimput.addEventListener("keydown", function(){
-  let textMeme = document.querySelector("#meme-text");
-  console.log(textMeme.innerHTML.length)
-  textMeme.innerHTML = " "
-  if(txtimput.value.length < 60){
+
+let textMeme = document.querySelector("#meme-text");
+
+txtimput.addEventListener("keyup", function(){
+  
+  let cont = textMeme.innerHTML.length
+
+  if(textMeme.innerHTML.length < 60){
     textMeme.innerHTML = txtimput.value;
   }
+  console.log(textMeme.innerHTML.length);  
 });
+
+//---------------------------------------------------------
+let clearText = document.querySelector("#limpar");
+clearText.addEventListener("click", function(){
+textMeme.innerHTML = "";
+txtimput.value = "";
+}); 
