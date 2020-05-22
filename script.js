0a82402e274c16e498d042acd3a06c244d9d1b94
rfreitasbatista <rfreitasbatista@gmail.com>
2020-05-22 18:38:48 -0300

    Finalizando inputs JS

--- script.js
+++ script.js
@@ -5,4 +5,17 @@ textInput.addEventListener("change", memeTextAdd)
 function memeTextAdd(){
 textOutput.innerHTML= textInput.value
 }
-
+let memeInput = document.getElementById("meme-insert")
+let memeImage = document.getElementById("meme-image")
+memeInput.addEventListener('change', function() {
+  readURL(this);
+});
+function readURL(input) {
+  if(input.files && input.files[0]) {
+    let reader = new FileReader();
+    reader.onload = function(event) {
+      memeImage.setAttribute('src', event.target.result);
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
