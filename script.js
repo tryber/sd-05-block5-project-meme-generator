5bfb9228bd162be836442c49e2e92c79db92d6f9
SamuelPPlab <samuelpp.s@hotmail.com>
2020-05-22 16:37:13 -0300

    extraindo a input e colocando ela na div da imagem.

---
+++ script.js
@@ -0,0 +1,8 @@
+let botaoCriar = document.getElementById("criar");
+
+botaoCriar.addEventListener("click", function() {
+
+    let texto = document.getElementById("text-input").value;
+    let textoimg = document.getElementById("meme-text");
+    textoimg.innerHTML = texto;
+})
