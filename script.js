b98a016b25f1999107bf4347d8071cb4bfe4f933
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-05-22 18:24:42 -0300

    add function that shows the image input on the page

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ let inputText = document.getElementById("text-input");
 let memeText = document.getElementById("meme-text");
 
 
-
 inputText.addEventListener("keyup", function () {
   memeText.innerHTML = inputText.value;
 });
 
+let inputImage = document.getElementById('meme-insert');
+let outputImage = document.getElementById('meme-image');
+// inputImage.addEventListener('input', function(){ 
+//     outputImage.src = inputImage
+// }
+// )
 
+inputImage.addEventListener("input", function(){
+  outputImage.src = URL.createObjectURL(this.files[0]);
+})
