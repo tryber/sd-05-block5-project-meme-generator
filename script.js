ca18645789bc24a3ece93305bc1decdb4f1d7027
SamuelPPlab <samuelpp.s@hotmail.com>
2020-05-22 17:05:22 -0300

    adicionada uma borda na meme container.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let botaoCriar = document.getElementById("criar");
 
 botaoCriar.addEventListener("click", function() {
-
     let texto = document.getElementById("text-input").value;
     let textoimg = document.getElementById("meme-text");
     textoimg.innerHTML = texto;
