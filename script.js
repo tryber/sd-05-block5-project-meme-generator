3bc0fe1ef53ee43e13ce18509314fb31d5cf2f3e
Christian Duessel <duessel@gmail.com>
2020-05-22 20:33:31 -0300

    bonus borda

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // colocar texto na foto:
 function colocatextomeme() {
-  let coloca = document.getElementById('text-input').value;
+  const coloca = document.getElementById('text-input').value;
   document.getElementsByTagName('h2')[0].innerHTML = coloca;
 }
 
@@ -13,12 +13,12 @@ document.getElementById('text-input').addEventListener('keyup', colocatextomeme)
 // colocar nova foto
 
 function trocaFoto(event) {
-  let selectedFile = event.target.files[0];
-  let reader = new FileReader();
+  const selectedFile = event.target.files[0];
+  const reader = new FileReader();
 
-  let imgtag = document.getElementById('meme-image');
+  const imgtag = document.getElementById('meme-image');
   
-  reader.onload = function(event) {
+    reader.onload = function (event) {
     imgtag.src = event.target.result;
   };
 
@@ -26,3 +26,35 @@ function trocaFoto(event) {
 }
 
 document.getElementById('meme-insert').addEventListener('change', trocaFoto);
+
+//botões de colocar borda
+let bobo = document.getElementById('meme-image-container');
+
+function bfire() {
+bobo.style.borderStyle= 'dashed'
+bobo.style.borderColor='red'
+bobo.style.borderWidth = '3px'
+}
+
+document.getElementById('fire').addEventListener('click', bfire);
+
+function bwater() {
+  bobo.style.borderStyle= 'double'
+  bobo.style.borderColor='blue'
+  bobo.style.borderWidth = '5px'
+  }
+  
+  document.getElementById('water').addEventListener('click', bwater);
+  
+  function bterra() {
+    bobo.style.borderStyle= 'groove'
+    bobo.style.borderColor='green'
+    bobo.style.borderWidth = '6px'
+    }
+    
+    document.getElementById('earth').addEventListener('click', bterra);
+
+
+
+
+
