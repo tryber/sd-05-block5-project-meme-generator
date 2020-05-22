bfd4994b1caba637881a876447ac60497d841832
rfreitasbatista <rfreitasbatista@gmail.com>
2020-05-22 20:24:08 -0300

    Adicionando mudança de bordas

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ function readURL(input) {
     reader.readAsDataURL(input.files[0]);
   }
 }
+
+let bdrChange = document.getElementById("meme-image-container")
+let fireBut = document.getElementById("fire");
+fireBut.addEventListener("click", function(){
+  bdrChange.style.border="3px dashed red"
+  console.log(bdrChange.style.border)
+})
+
+let waterBut = document.getElementById("water");
+waterBut.addEventListener("click", function(){
+  bdrChange.style.border="5px double blue"
+  console.log(bdrChange.style.border)
+})
+
+let earthBut = document.getElementById("earth");
+earthBut.addEventListener("click", function(){
+  bdrChange.style.border="6px groove green"
+  console.log(bdrChange.style.border)
+})
