6b4a14c910e368aa7f069796a19a0377348f20a4
pauloricardoz <trybe.przt@gmail.com>
2020-05-22 19:26:06 -0300

    text size and color2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let text_input = document.getElementById("text-input")
+let text_input = document.getElementById("text-input");
 text_input.addEventListener("keyup", function () {
   let text = text_input.value;
   if (text.length > 60) {
@@ -7,3 +7,15 @@ text_input.addEventListener("keyup", function () {
   }
   document.getElementById("meme-text").innerHTML = text_input.value
 });
+
+// The image uploaded by the user is loaded inside the container
+function readImage(event) {
+  if (event.target.files && event.target.files[0]) {
+    const file = new FileReader();
+    file.onload = function (e) {
+      document.getElementById('meme-image').src = e.target.result;
+    };
+    file.readAsDataURL(event.target.files[0]);
+  }
+}
+document.querySelector('#meme-insert').addEventListener('change', readImage, false);
