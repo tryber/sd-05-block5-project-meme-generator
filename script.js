d39c7f6ae5366cfd3acdfba425d4293b45b88669
pauloricardoz <trybe.przt@gmail.com>
2020-05-22 18:21:34 -0300

    Returning to do

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let text_input = document.getElementById("text-input")
-text_input.addEventListener("keyup",function(){
-  document.getElementById("meme-text").innerHTML=text_input.value
-  });
+text_input.addEventListener("keyup", function () {
+  let text = text_input.value;
+  if (text.length > 60) {
+    alert("texto muito grande");
+    text_input.value=text.substr(0,60);
+  }
+  document.getElementById("meme-text").innerHTML = text_input.value
+});
