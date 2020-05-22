1333f8d583726b2305cb497763a881bd628bb2d4
pedrohcalado <pedrocalado22@gmail.com>
2020-05-22 15:40:25 -0300

    Adjust txt variable

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //create variable to store text input
 let txt = document.querySelector("#text-input");
 
-//add the text input to the div meme-text
-document.querySelector("#meme-text").innerHTML = txt.value;
+//add a event listener to add the text to the div meme-text when the user types it
+txt.addEventListener("keyup", function() {
+  document.querySelector("#meme-text").innerHTML = txt.value;
+})
