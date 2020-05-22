aa93e5989cdd7e179fe94debaf50177e8689bae1
Felipe Vieira <flpvieira86@gmail.com>
2020-05-22 16:11:44 -0300

    parte 2 - criado container para mostrar a imagem, adicionado input para imagem e ajustado o height e width para imagem se ajustar ao container

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // definição de variaveis
-
+let memeImage = document.getElementById("meme-image");
 let textInput = document.getElementById("text-input");
 let caixaTxtDoMeme = document.getElementById("meme-text");
+let imgInput = document.getElementById("meme-insert");
 
 // eventListeners
 textInput.addEventListener("keyup", criaTxtDoMeme);
-
+imgInput.addEventListener("input", function(){
+  memeImage.src = URL.createObjectURL(this.files[0]);
+})
 // Functions
 
 function criaTxtDoMeme() {
