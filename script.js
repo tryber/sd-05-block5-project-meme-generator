ece5ba33360596f6e10ee68829a961a92832b7be
Mau <mauricio.sdjr@gmail.com>
2020-05-22 19:14:20 -0300

    add script src

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+   //Meme Text
+   let memeText = document.getElementById("meme-text");
+   let textInput = document.getElementById("text-input");
+   
+   textInput.addEventListener("keyup", function () {
+    memeText.innerHTML = textInput.value;
+   });
+   
+   
+   //Meme Image
+   let memeImage = document.getElementById("meme-image");
+   let memeInsert = document.getElementById("meme-insert");
+   
+   memeInsert.addEventListener("input", function() {
+       memeImage.src = URL.createObjectURL(this.files[0]);
+   })
