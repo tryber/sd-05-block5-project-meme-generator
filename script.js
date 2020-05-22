17463c0192b804ef8db0692c7f853714bc5584e9
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-05-22 19:27:38 -0300

    alteraçao de nomes de pasta code climate

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.querySelector('#meme-insert').addEventListener('change', readImage, fal
 
 // use the buttons to change the color of the container's border
 function changeColor1() {
-  document.getElementById('meme-image-container').style.border = '3px double red';
+  document.getElementById('meme-image-container').style.border = '3px dashed red';
 }
 const buttonFire = document.getElementById('fire');
 buttonFire.addEventListener('click', changeColor1);
@@ -34,34 +34,34 @@ const buttonWater = document.getElementById('water');
 buttonWater.addEventListener('click', changeColor2);
 
 function changeColor() {
-  document.getElementById('meme-image-container').style.border = '6px double green';
+  document.getElementById('meme-image-container').style.border = '6px groove green';
 }
 const buttonEarth = document.getElementById('earth');
 buttonEarth.addEventListener('click', changeColor);
 
 // Load memes the are already done
-const loadMeme1 = document.getElementById('meme-1').firstChild;
+const loadMeme1 = document.getElementById('meme-1');
 function loadMemeDone1() {
   document.getElementById('meme-image').src = loadMeme1.src;
   document.getElementById('meme-text').innerHTML = 'É o que que está acontencendo?';
 }
 loadMeme1.addEventListener('click', loadMemeDone1);
 
-const loadMeme2 = document.getElementById('meme-2').firstChild;
+const loadMeme2 = document.getElementById('meme-2');
 function loadMemeDone2() {
   document.getElementById('meme-image').src = loadMeme2.src;
   document.getElementById('meme-text').innerHTML = 'Ehhhh... Melhor não';
 }
 loadMeme2.addEventListener('click', loadMemeDone2);
 
-const loadMeme3 = document.getElementById('meme-3').firstChild;
+const loadMeme3 = document.getElementById('meme-3');
 function loadMemeDone3() {
   document.getElementById('meme-image').src = loadMeme3.src;
   document.getElementById('meme-text').innerHTML = 'Cri cri cri';
 }
 loadMeme3.addEventListener('click', loadMemeDone3);
 
-const loadMeme4 = document.getElementById('meme-4').firstChild;
+const loadMeme4 = document.getElementById('meme-4');
 function loadMemeDone4() {
   document.getElementById('meme-image').src = loadMeme4.src;
   document.getElementById('meme-text').innerHTML = 'Não peguei a referencia';
