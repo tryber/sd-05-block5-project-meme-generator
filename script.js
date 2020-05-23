52549c7514b122e1e03882d3a8437335a414da85
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-05-23 19:03:35 -0300

    Ainda falta colocar o texto

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-  window.onload = function (){
+  window.onload = function() {
+    document.getElementById("meme-insert").addEventListener("change", carregaArquivo);
+    document.getElementById("text-input").addEventListener("keyup", imprimeTexto);
+
+    
+
+    function carregaArquivo(event) {
+      let saida = document.getElementById("meme-image");
+      saida.src = URL.createObjectURL(event.target.files[0]);
+      saida.onload = function() {
+        URL.revokeObjectURL(saida.src);// libera a memoria depois que carregar - o onload carrega logo após ter sido carregado o arquivo todo.
+      }
+    }
+
+    function imprimeTexto() {
+      let texto = document.getElementById("text-input").value;
+      console.log(texto);
+      let posTexto = document.getElementById("meme-text");
+      posTexto.innerHTML = texto;
+    }
   }
