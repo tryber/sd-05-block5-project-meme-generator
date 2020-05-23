fd5154b28f532901a2e789de740f914ffab7b262
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-05-22 23:20:04 -0300

    Corrigindo  alguns warnings do code climate e mudando um pouco o javascript.

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const memeImage = document.getElementById('meme-image');
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 
-memeInput.addEventListener('input', function () {
-  memeImage.src = URL.createObjectURL(this.files[0]);
-});
+function insertImage(event) {
+  if (event.target.files && event.target.files[0]) {
+    const img = new FileReader();
+    img.onload = function (ev) {
+      memeImage.src = ev.target.result;
+    };
+    img.readAsDataURL(event.target.files[0]);
+  }
+}
+memeInput.addEventListener('change', insertImage, false);
 
 textInput.addEventListener('keyup', function () {
   memeText.innerText = this.value;
