cec53deca08fbd73d283c989419a4e55da93f70e
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-23 12:44:12 -0300

    corrigindo erros do codeclimate

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let memeText = "";
-
 let textInput = document.querySelector("#text-input");
 textInput.addEventListener("change", typeAndScream);
 
 function typeAndScream() {
-    memeText = document.getElementById("text-input").value;
+  let memeText = document.getElementById("text-input").value;
   document.querySelector("#meme-text").innerHTML = memeText;
 }
