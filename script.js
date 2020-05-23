0434bfccbf5586431eb1a6c09aa1b784115c8e28
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-05-22 22:46:42 -0300

    warnings do  code climate.

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-const memeInput = document.getElementById("meme-input");
-const memeImage = document.getElementById("meme-image");
-const textInput = document.getElementById("text-input");
-const memeText = document.getElementById("meme-text");
-const memeImageContainer = document.getElementById("meme-image-container");
+const memeInput = document.getElementById('meme-input');
+const memeImage = document.getElementById('meme-image');
+const textInput = document.getElementById('text-input');
+const memeText = document.getElementById('meme-text');
 
-memeInput.addEventListener("input", function(){
+memeInput.addEventListener('input', function(){
   memeImage.src = URL.createObjectURL(this.files[0]);
 });
-textInput.addEventListener("keyup", function() {
+
+textInput.addEventListener('keyup', function() {
   memeText.innerText = this.value;
 });
-
-// Funções
-
