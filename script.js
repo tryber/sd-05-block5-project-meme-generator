57b41289b3bf950cc1902e2f15f456aec3cd5763
Larissa Palombo <larissampalombo@gmail.com>
2020-05-23 18:00:14 -0300

    inicio js

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    let texto = document.querySelector('#meme-text');
+    let imagem = document.querySelector('#meme-image');
+    let textoEntrada = document.getElementsById('#text-input');
+    let imagemEntrada  = document.querySelector('#meme-insert');
+
+    
+}
