ace44f91e118d89da33ce1dfa38a64e44a41ad8c
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-05-22 22:57:50 -0300

    Mais warnings do  code climate.

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const memeImage = document.getElementById('meme-image');
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 
-memeInput.addEventListener('input', function(){
+memeInput.addEventListener('input', function () {
   memeImage.src = URL.createObjectURL(this.files[0]);
 });
 
-textInput.addEventListener('keyup', function() {
+textInput.addEventListener('keyup', function () {
   memeText.innerText = this.value;
 });
