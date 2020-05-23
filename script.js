98d7dbd91eba3a18ffbed994c5b5f865adbd1713
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-05-23 12:19:00 -0300

    image inserted

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 let imagem = document.querySelector("#img2");
-let texBox = document.querySelector("#text-input");
+let textBox = document.querySelector("#text-input");
 let div = document.querySelector("div");
-let imgTag = document.createElement("img")
-div.appendChild(imgTag);
+let imgTag = document.createElement("img");
+let text = document.querySelector('.text');
+let secondDiv = document.createElement('div');
+
+
 
 function insertImage () {
+    div.appendChild(imgTag);
     imgTag.src = URL.createObjectURL(imagem.files[0]);
 }
+function insertText() {
+    secondDiv.className = 'insert-text'
+    document.querySelector('img').appendChild(secondDiv);
+    let divText = document.querySelector('.insert-text');
+    divText.innerText = textBox.value;
+    divText.style.position = "absolute";
+}   
 
 imagem.addEventListener('change', insertImage);
+textBox.addEventListener('change', insertText);
+
+
 
