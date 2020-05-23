11e16ca870eaf3af1b72a6a317818838c3f9a5ea
pedrohcalado <pedrocalado22@gmail.com>
2020-05-23 07:20:37 -0300

    Adjustments to match code climate

--- script.js
+++ script.js
@@ -16,38 +16,38 @@ function loadFile(event) {
 }
 
 // create variables to get the buttons that style borders and the image
-const earthButton = document.getElementById("earth");
-const fireButton = document.getElementById("fire");
-const waterButton = document.getElementById("water");
-const ctnr = document.getElementById("meme-image-container");
+const earthButton = document.getElementById('earth');
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const ctnr = document.getElementById('meme-image-container');
 
-// add event listener to the buttons that change border and apply the style to meme-image-container's border
-earthButton.addEventListener("click", function () {
-  ctnr.style.border = "6px groove green";
+// add event listener to the buttons and apply the style to meme-image-container's border
+earthButton.addEventListener('click', function () {
+  ctnr.style.border = '6px groove green';
 });
-fireButton.addEventListener("click", function () {
-  ctnr.style.border = "3px dashed red";
+fireButton.addEventListener('click', function () {
+  ctnr.style.border = '3px dashed red';
 });
-waterButton.addEventListener("click", function () {
-  ctnr.style.border = "5px double blue";
+waterButton.addEventListener('click', function () {
+  ctnr.style.border = '5px double blue';
 });
 
 // create variables to get each meme sample
-const meme1 = document.getElementById("meme-1");
-const meme2 = document.getElementById("meme-2");
-const meme3 = document.getElementById("meme-3");
-const meme4 = document.getElementById("meme-4");
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
 
 // add event listener to select the image
-meme1.addEventListener("click", function () {
+meme1.addEventListener('click', function () {
   document.getElementById("meme-image").src = meme1.src;
 });
-meme2.addEventListener("click", function () {
+meme2.addEventListener('click', function () {
   document.getElementById("meme-image").src = meme2.src;
 });
-meme3.addEventListener("click", function () {
-  document.getElementById("meme-image").src = meme3.src;
+meme3.addEventListener('click', function () {
+  document.getElementById('meme-image').src = meme3.src;
 });
-meme4.addEventListener("click", function () {
-  document.getElementById("meme-image").src = meme4.src;
+meme4.addEventListener('click', function () {
+  document.getElementById('meme-image').src = meme4.src;
 });
