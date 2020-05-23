5c6368bf87a8602b676133e11255d3843c51ca4a
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-05-23 12:34:21 -0300

    text box position type changed

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ let imagem = document.querySelector("#img2");
 let textBox = document.querySelector("#text-input");
 let div = document.querySelector("div");
 let imgTag = document.createElement("img");
-let text = document.querySelector('.text');
-let secondDiv = document.createElement('div');
+let text = document.querySelector('#text');
 
 
 
@@ -12,11 +11,10 @@ function insertImage () {
     imgTag.src = URL.createObjectURL(imagem.files[0]);
 }
 function insertText() {
-    secondDiv.className = 'insert-text'
-    document.querySelector('img').appendChild(secondDiv);
-    let divText = document.querySelector('.insert-text');
-    divText.innerText = textBox.value;
-    divText.style.position = "absolute";
+    let insert = document.querySelector('#text');
+    insert.style.position = "absolute";
+    insert.innerText = textBox.value;
+    
 }   
 
 imagem.addEventListener('change', insertImage);
