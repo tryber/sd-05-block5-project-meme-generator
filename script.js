6ae99f4e34b3e461b692a926b780a126d89fd2d2
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-05-22 22:35:25 -0300

    Corrigindo o CSS, alerando umas coisas no HTML e retirando codigo desnecessário do JavaScript.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const memeInput = document.getElementById("meme-insert");
+const memeInput = document.getElementById("meme-input");
 const memeImage = document.getElementById("meme-image");
+const textInput = document.getElementById("text-input");
+const memeText = document.getElementById("meme-text");
+const memeImageContainer = document.getElementById("meme-image-container");
+
 memeInput.addEventListener("input", function(){
   memeImage.src = URL.createObjectURL(this.files[0]);
 });
+textInput.addEventListener("keyup", function() {
+  memeText.innerText = this.value;
+});
+
+// Funções
+
