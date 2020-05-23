db4a701f9f1ac57fb56fcf74b3263e2a30f67264
César Alves <alvescesar@hotmail.com>
2020-05-22 22:44:01 -0300

    Added user text input, fixed image input

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let textInput = function(event) {
+  let memeText = document.getElementById("meme-text");
+  memeText.innerHTML = event.target.value;
+}
 
 let imageInput = function(event) {
   let memeImage = document.getElementById("meme-image");
