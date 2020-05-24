086eca66a8c56a74bd151789bce149ebf40a08ab
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-05-24 15:10:50 -0300

    Added CSS conditions about text and image.

--- script.js
+++ script.js
@@ -5,20 +5,21 @@ function insert() {
 
 // porque usar "event" como parâmetro?
 let loadImage = function(event) {
-  let imageOutput = document.getElementById("imageOutput");
+  let memeImage = document.getElementById("meme-image");
   // O método estático URL.createObjectURL() cria um DOMString
   // contendo uma URL que representa o objeto fornecido no parâmetro.
   // O tempo de vida da URL está vinculado ao documento na janela
   // em que foi criado.
   // O novo URL do objeto representa o objeto File ou
   // Blob(arquivo lido como texto ou binário) especificado.
-  imageOutput.src = URL.createObjectURL(event.target.files[0]);
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
   // SRC = Se o valor do atributo type for image,
   // este atributo especifica uma URL para a localização de uma
   // imagem a ser exibida no botão gráfico; caso contrário,
   // este atributo é ignorado.
-  imageOutput.onload = function() {
+  memeImage.onload = function() {
     // onload para deixar a box em branco enquanto espera um input.
-    URL.revokeObjectURL(imageOutput.src) // Para resetar a memória e não ficar imagens gravadas quando trocá-las.
+    URL.revokeObjectURL(memeImage.src) // Para resetar a memória e não ficar imagens gravadas quando trocá-las.
   }
-};
+}
+
