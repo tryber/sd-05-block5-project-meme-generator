f06257313759bc08c87f4321ac6b1f4cfff730e2
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-05-23 21:46:57 -0300

    coding issues #2

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-  window.onload = function() {
-    let memeInsert = document.getElementById("meme-insert");
+  window.onload = function () {
+    const memeInsert = document.getElementById('meme-insert');
     memeInsert.addEventListener("change", carregaArquivo);
 
-    let textInput = document.getElementById("text-input");
-    textInput.addEventListener("keyup", imprimeTexto);
+    const textInput = document.getElementById('text-input');
+    textInput.addEventListener('keyup', imprimeTexto);
 
     function carregaArquivo(event) {
-      const saida = document.getElementById("meme-image");
+      const saida = document.getElementById('meme-image');
       saida.src = URL.createObjectURL(event.target.files[0]);
-      saida.onload = function() {
+      saida.onload = function () {
         URL.revokeObjectURL(saida.src);
-        // libera a memoria depois que carregar - o onload carrega logo após ter sido carregado o arquivo todo.
+        // libera a memoria depois que carregar - o onload carrega 
+        // logo após ter sido carregado o arquivo todo.
       }
     }
 
     function imprimeTexto() {
-      let texto = document.getElementById("text-input");
+      let texto = document.getElementById('text-input');
       texto = texto.value;
-      console.log(texto);
-      let posTexto = document.getElementById("meme-text");
+      const posTexto = document.getElementById('meme-text');
       posTexto.innerHTML = texto;
     }
   }
