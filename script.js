16e5031675f2ebb6b5626f0cefec2e8d45dd84ff
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-05-23 21:18:55 -0300

    inclusão de botões no html, com os respectivos scripts para mudar moldura + ajustes de estilo

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ const memeImage = document.getElementById('meme-image');
 memeInsert.addEventListener('input', function () {
   memeImage.src = URL.createObjectURL(this.files[0]);
 });
+
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
+const imageContainer = document.getElementById('meme-image-container');
+
+function borda () {
+  if(event.target === fire) {
+    imageContainer.style.borderColor = 'red';
+    imageContainer.style.borderWidth = '3px';
+    imageContainer.style.borderStyle = 'dashed';
+  }  
+
+  if(event.target === water) {
+    imageContainer.style.borderColor = 'blue';
+    imageContainer.style.borderWidth = '5px';
+    imageContainer.style.borderStyle = 'double';
+  }  
+
+  if(event.target === earth) {
+    imageContainer.style.borderColor = 'green';
+    imageContainer.style.borderWidth = '6px';
+    imageContainer.style.borderStyle = 'groove';
+  }  
+}
+
+fire.addEventListener('click', borda);
+water.addEventListener('click', borda);
+earth.addEventListener('click', borda);
