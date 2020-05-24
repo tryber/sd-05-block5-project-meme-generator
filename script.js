f5fae4557b42edb95a3d74d9bf828f2acfc32af9
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-05-24 14:40:33 -0300

    Add valores de entrada no JS e funçao para add texto a img

---
+++ script.js
@@ -0,0 +1,14 @@
+//Variáveis de entrada 
+const memeImage = document.getElementById('meme-image');
+const textoInput = document.getElementById('text-input');
+const containerLimite = document.getElementById('meme-image-container');
+const textoMeme = document.getElementById('meme-text');
+const inputImg = document.getElementById('meme-insert');
+
+/*Definição da função para adicionar o texto sobre a imagem
+verifica se o evento do target for igual ao texto de entrada,
+atribui o valor da variável textoInput para o textoMeme*/
+function memeText() {
+  if (event.target === textoInput) {
+    textoMeme.innerText = textoInput.value; 
+  }
