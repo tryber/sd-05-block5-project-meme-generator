79ec1acb37154ba365446da4bea5d4f5dd3e8837
SamuelPPlab <samuelpp.s@hotmail.com>
2020-05-24 01:24:32 -0300

    troquei o id do p do texto.

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let botaoCriar = document.getElementById("criar");
 
 botaoCriar.addEventListener("click", function() {
     let texto = document.getElementById("text-input").value;
-    let textoimg = document.getElementById("meme-text");
+    let textoimg = document.getElementById(id="meme-image-container");
     textoimg.innerHTML = texto;
 })
