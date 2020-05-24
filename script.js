1218ea38bc98147f68b618d2895dde87b1edfba1
luca <luucadecastro@gmail.com>
2020-05-24 16:49:05 -0300

    finalizando projeto

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const memeUm = document.getElementById('meme1');
 const memeDois = document.getElementById('meme2');
 const memeTres = document.getElementById('meme3');
 const memeQuatro = document.getElementById('meme4');
+const caixaTxtDoMeme = document.getElementById('meme-text');
 
 function caixaTxt() {
   memeText.textContent = textInput.value;
@@ -36,46 +37,21 @@ function changeBorder() {
 function changePic() {
   if (event.target === memeQuatro) {
     memeImage.src = '/imgs/meme4.png';
-    caixaTxtDoMeme4.style.left = '40px';
-    caixaTxtDoMeme4.style.top = '80px';
-    caixaTxtDoMeme4.style.width = '340px';
-    caixaTxtDoMeme4.style.visibility = 'visible';
-    caixaTxtDoMeme4.style.left = '40px';
-    caixaTxtDoMeme4.style.top = '420px';
-    caixaTxtDoMeme4.style.width = '340px';
   }
   if (event.target === memeTres) {
     memeImage.src = '/imgs/meme3.png';
-    caixaTxtDoMeme3.style.left = '40px';
-    caixaTxtDoMeme3.style.top = '80px';
-    caixaTxtDoMeme3.style.width = '340px';
-    caixaTxtDoMeme3.style.visibility = 'visible';
-    caixaTxtDoMeme3.style.left = '40px';
-    caixaTxtDoMeme3.style.top = '400px';
-    caixaTxtDoMeme3.style.width = '340px';
   }
   if (event.target === memeDois) {
     memeImage.src = '/imgs/meme2.png';
-    caixaTxtDoMeme2.style.left = '40px';
-    caixaTxtDoMeme2.style.top = '80px';
-    caixaTxtDoMeme2.style.width = '340px';
-    caixaTxtDoMeme2.style.visibility = 'hidden';
-    }
+  }
   if (event.target === memeUm) {
     memeImage.src = '/imgs/meme1.png';
-    caixaTxtDoMeme1.style.left = '10px';
-    caixaTxtDoMeme1.style.top = '200px';
-    caixaTxtDoMeme1.style.width = '200px';
-    caixaTxtDoMeme1.style.visibility = 'visible';
-    caixaTxtDoMeme1.style.left = '200px';
-    caixaTxtDoMeme1.style.top = '300px';
-    caixaTxtDoMeme1.style.width = '200px';
   }
 }
 
 textInput.addEventListener('keyup', caixaTxt);
 memeInsert.addEventListener('input', function () {
-memeImage.src = URL.createObjectURL(this.files[0]);
+  memeImage.src = URL.createObjectURL(this.files[0]);
 });
 fire.addEventListener('click', changeBorder);
 water.addEventListener('click', changeBorder);
