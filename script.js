ebbce67e28ff2289eb3561c8e41059545cc78cfe
marinabrcls <barcelos.marina@gmail.com>
2020-05-24 19:09:45 -0300

    Mudanças nas ids e classes do CSS

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 //Variáveis
-
 let textInput = document.getElementById("text-input");
+let imageInput = document.getElementById("meme-insert");
 let memeText = document.getElementById("meme-text");
 let memeImage = document.getElementById("meme-image");
-let imageInput = document.getElementById("meme-insert");
+let imageContainer = document.getElementById("meme-image-container");
+
 
 //Funções
 function insertMemeText() {
-    memeText.innerText = textInput.value;
+  memeText.innerText = textInput.value;
+}
+function insertMemeImage() {
+  memeImage.src=URL.createObjectURL(this.files[0]);
 }
 
 //Event Listners
+textInput.addEventListener("keyup", insertMemeText);
+imageInput.addEventListener("input", insertMemeImage);
