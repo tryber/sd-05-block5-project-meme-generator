bad340bdfa30c028075645623583ba6f838da8b8
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-05-24 15:36:36 -0300

    code climate ta querendo meu amor

--- script.js
+++ script.js
@@ -2,49 +2,48 @@ const inputText = document.getElementById('text-input');
 
 const memeText = document.getElementById('meme-text');
 
-
-inputText.addEventListener("keyup", function () {
+inputText.addEventListener('keyup', function () {
   memeText.innerHTML = inputText.value;
 });
 
 const inputImage = document.getElementById('meme-insert');
 const outputImage = document.getElementById('meme-image');
 
-inputImage.addEventListener('input', function(){ 
+inputImage.addEventListener('input', function () {
   outputImage.src = URL.createObjectURL(this.files[0]);
-})
+});
 
-const meme1 =document.getElementById('meme-1');
-meme1.addEventListener('click', function() {
+const meme1 = document.getElementById('meme-1');
+meme1.addEventListener('click', function () {
   outputImage.src = 'imgs/meme1.png';
-})
+});
 
-const meme2 =document.getElementById('meme-2');
-meme2.addEventListener('click', function() {
+const meme2 = document.getElementById('meme-2');
+meme2.addEventListener('click', function () {
   outputImage.src = 'imgs/meme2.png';
-})
+});
 
-const meme3 =document.getElementById('meme-3');
-meme3.addEventListener('click', function() {
+const meme3 = document.getElementById('meme-3');
+meme3.addEventListener('click', function () {
   outputImage.src = 'imgs/meme3.png';
-})
+});
 
-const meme4 =document.getElementById('meme-4');
-meme4.addEventListener('click', function(){
+const meme4 = document.getElementById('meme-4');
+meme4.addEventListener('click', function () {
   outputImage.src = 'imgs/meme4.png';
-})
+});
 
 const fire = document.getElementById('fire');
 const water = document.getElementById('water');
 const earth = document.getElementById('earth');
 const container = document.getElementById('meme-image-container');
 
-fire.addEventListener('click', function(){
+fire.addEventListener('click', function () {
   container.style.border = '3px dashed red';
-})
-water.addEventListener('click', function(){
+});
+water.addEventListener('click', function () {
   container.style.border = '5px double blue';
-})
-earth.addEventListener('click', function(){
+});
+earth.addEventListener('click', function () {
   container.style.border = '6px groove green';
-})
+});
