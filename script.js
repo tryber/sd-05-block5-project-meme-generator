ff42d4853bda97767e7bd789b7af32bb15881ae2
Larissa Palombo <larissampalombo@gmail.com>
2020-05-24 14:50:36 -0300

    consertando html

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
- window.onload {
+
     const texto = document.getElementById('meme-text');
     const imagem = document.getElementById('meme-image');
     const textoEntrada = document.getElementsById('text-input');
     const imagemEntrada  = document.getElementById('meme-insert');
 
-    texto.addEventListener('keyup1', printTexto)
-    imagem.addEventListener('input', printImg)
+    textoEntrada.addEventListener('keyup', printTexto)
+    imagemEntrada.addEventListener('input', printImg)
 
     function printTexto (){
         texto.innerHTML = textoEntrada.value;
-    }
+    };
 
     function printImg (){
         imagem.src = URL.createObjectURL(this.files[0]);
-    }
- }
+    };
