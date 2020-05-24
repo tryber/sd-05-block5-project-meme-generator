6c8f2254406bab8207ab63e28e2e43509e3c56a2
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-05-23 22:26:16 -0300

    climate issues #3

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-  window.onload = function () {
-    const memeInsert = document.getElementById('meme-insert');
-    memeInsert.addEventListener("change", carregaArquivo);
-
-    const textInput = document.getElementById('text-input');
-    textInput.addEventListener('keyup', imprimeTexto);
-
-    function carregaArquivo(event) {
-      const saida = document.getElementById('meme-image');
-      saida.src = URL.createObjectURL(event.target.files[0]);
-      saida.onload = function () {
-        URL.revokeObjectURL(saida.src);
-        // libera a memoria depois que carregar - o onload carrega 
-        // logo após ter sido carregado o arquivo todo.
-      }
-    }
-
-    function imprimeTexto() {
-      let texto = document.getElementById('text-input');
-      texto = texto.value;
-      const posTexto = document.getElementById('meme-text');
-      posTexto.innerHTML = texto;
+window.onload = function () {
+  function carregaArquivo(event) {
+    const saida = document.getElementById('meme-image');
+    saida.src = URL.createObjectURL(event.target.files[0]);
+    saida.onload = function () {
+      URL.revokeObjectURL(saida.src);
+// libera a memoria depois que carregar - o onload carrega 
+// logo após ter sido carregado o arquivo todo.
     }
   }
+
+  function imprimeTexto() {
+    let texto = document.getElementById('text-input');
+    texto = texto.value;
+    const posTexto = document.getElementById('meme-text');
+    posTexto.innerHTML = texto;
+  }
+
+  const memeInsert = document.getElementById('meme-insert');
+  memeInsert.addEventListener('change', carregaArquivo);
+
+  const textInput = document.getElementById('text-input');
+  textInput.addEventListener('keyup', imprimeTexto);
+}
