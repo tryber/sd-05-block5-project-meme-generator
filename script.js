603631ee4a57f5f27d69c7041b25f10680bc5ed8
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-05-24 14:41:19 -0300

    add functionality by clicking it in a meme image

--- script.js
+++ script.js
@@ -18,6 +18,26 @@ inputImage.addEventListener("input", function(){
   outputImage.src = URL.createObjectURL(this.files[0]);
 })
 
+let meme1 =document.getElementById('meme-1');
+meme1.addEventListener('click', function(){
+  outputImage.src = 'imgs/meme1.png'
+})
+
+let meme2 =document.getElementById('meme-2');
+meme2.addEventListener('click', function(){
+  outputImage.src = 'imgs/meme2.png'
+})
+
+let meme3 =document.getElementById('meme-3');
+meme3.addEventListener('click', function(){
+  outputImage.src = 'imgs/meme3.png'
+})
+
+let meme4 =document.getElementById('meme-4');
+meme4.addEventListener('click', function(){
+  outputImage.src = 'imgs/meme4.png'
+})
+
 let fire = document.getElementById('fire');
 let water = document.getElementById('water');
 let earth = document.getElementById('earth');
