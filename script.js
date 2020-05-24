5c1a0bea06a638e799811380f78caf3c2ccec20e
luca <luucadecastro@gmail.com>
2020-05-24 12:18:15 -0300

    semantica corrigida, cor do texto modificada

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-var setTextInput = function(event) {
-  const input = document.getElementById("text-input");
-  const text= document.getElementById("meme-text");
+let setTextInput = function (event) {
+  const input = document.getElementById('text-input');
+  const text= document.getElementById('meme-text');
 
-  text.innerHTML = input.value; 
- }
+  text.innerHTML = input.value;
+};
 
-var loadFile = function(event) {
-var output = document.getElementById('output');
+let loadFile = function (event) {
+  let output = document.getElementById('output');
   output.src = URL.createObjectURL(event.target.files[0]);
   output.onload = function() {
-  URL.revokeObjectURL(output.src) // free memory
-}
+    URL.revokeObjectURL(output.src); // free memory
+  };
+};
 
-var borderChange = function(event) {
-  var btn = document.getElementById("earth");
-  var brdr = document.getElementById("meme-image-container");
-
-  
-}
+let borderChange = function (event) {
+  let btn = document.getElementById('earth');
+  let brdr = document.getElementById('meme-image-container');
 };
