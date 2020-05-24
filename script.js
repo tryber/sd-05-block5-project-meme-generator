b477e591e22c8bb7c7a9ef1a0bee7569581cd525
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-05-24 15:25:24 -0300

    Mudança no JS

--- script.js
+++ script.js
@@ -5,19 +5,11 @@ let textoInput = document.getElementById('text-input');
 let memeImage = document.getElementById('meme-image');
 let inputImg = document.getElementById('meme-insert');
 
-/*Definição da função para adicionar o texto sobre a imagem
-verifica se o evento do target for igual ao texto de entrada,
-atribui o valor da variável textoInput para o textoMeme*/
-function memeText() {
-  if (event.target === textoInput) {
-    textoMeme.innerText = textoInput.value; 
-  }
+//Interação com o usuário, adição de texto e imagem
+textoInput.addEventListener('keyup', function(){
+  textoMeme.innerHTML = textoInput.nodeValue;
+});
 
-//Interação com o usuário, adição dos eventos
-textoInput.addEventListener('keyup', textoMeme);
-
-//Função anônima para colocar a img selecionada do pc 
-//como a img de entrada
 inputImg.addEventListener('input', function () {
   memeImage.src = URL.createObjectURL(this.files[0]);
 });
