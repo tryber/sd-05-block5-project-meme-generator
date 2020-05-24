f66d1d926d01ee3078fbf76ac944442e8fcfc304
Erlingsjunior <erlingsjunior@gmail.com>
2020-05-24 02:22:56 -0300

    input text finalizado

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-  let valor = document.getElementById("text-input").value;
+  //let valor = document.getElementById("text-input").value;
   
-  document.querySelector("#meme-text").innerText = valor;
+  //document.querySelector("#meme-text").innerText = valor;
+
+  let input1 = document.getElementById("text-input");
+  let input2 = document.querySelector("#meme-text");
+
+  function trocaTexto(){
+    input2.innerText = input1.value
+  }
+
+  input1.addEventListener('keyup', trocaTexto);
