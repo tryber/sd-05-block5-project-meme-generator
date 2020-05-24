641944b86ba94b83d20c5c73e6280543b7f40249
Luiz Araujo <araujo.script@gmail.com>
2020-05-24 19:48:01 -0300

    input

---
+++ script.js
@@ -0,0 +1,5 @@
+const memeInput = document.getElementById("meme-Input");
+const memeImage = document.getElementaryById("meme-Image");
+memeInput.addEventListener("input, function()") {
+    memeImage.src = URL.createObjectURL(this.memeImage[0]);
+}
