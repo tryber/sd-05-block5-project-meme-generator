cc5ee0705381da9efa6bbf1a82117042a89450ba
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-05-24 15:26:47 -0300

    entrando no clima com code climate

--- script.js
+++ script.js
@@ -1,47 +1,43 @@
-let inputText = document.getElementById("text-input");
+const inputText = document.getElementById('text-input');
 
-let memeText = document.getElementById("meme-text");
+const memeText = document.getElementById('meme-text');
 
 
 inputText.addEventListener("keyup", function () {
   memeText.innerHTML = inputText.value;
 });
 
-let inputImage = document.getElementById('meme-insert');
-let outputImage = document.getElementById('meme-image');
-// inputImage.addEventListener('input', function(){ 
-//     outputImage.src = inputImage
-// }
-// )
+const inputImage = document.getElementById('meme-insert');
+const outputImage = document.getElementById('meme-image');
 
-inputImage.addEventListener("input", function(){
+inputImage.addEventListener('input', function(){ 
   outputImage.src = URL.createObjectURL(this.files[0]);
 })
 
-let meme1 =document.getElementById('meme-1');
-meme1.addEventListener('click', function(){
-  outputImage.src = 'imgs/meme1.png'
+const meme1 =document.getElementById('meme-1');
+meme1.addEventListener('click', function() {
+  outputImage.src = 'imgs/meme1.png';
 })
 
-let meme2 =document.getElementById('meme-2');
-meme2.addEventListener('click', function(){
-  outputImage.src = 'imgs/meme2.png'
+const meme2 =document.getElementById('meme-2');
+meme2.addEventListener('click', function() {
+  outputImage.src = 'imgs/meme2.png';
 })
 
-let meme3 =document.getElementById('meme-3');
-meme3.addEventListener('click', function(){
-  outputImage.src = 'imgs/meme3.png'
+const meme3 =document.getElementById('meme-3');
+meme3.addEventListener('click', function() {
+  outputImage.src = 'imgs/meme3.png';
 })
 
-let meme4 =document.getElementById('meme-4');
+const meme4 =document.getElementById('meme-4');
 meme4.addEventListener('click', function(){
-  outputImage.src = 'imgs/meme4.png'
+  outputImage.src = 'imgs/meme4.png';
 })
 
-let fire = document.getElementById('fire');
-let water = document.getElementById('water');
-let earth = document.getElementById('earth');
-let container = document.getElementById('meme-image-container');
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
+const container = document.getElementById('meme-image-container');
 
 fire.addEventListener('click', function(){
   container.style.border = '3px dashed red';
