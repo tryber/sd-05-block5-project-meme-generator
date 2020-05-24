f2f9ef6e9561ba9065370ce8098e5b9736d3fc5d
matambrosio <matmagamb1@gmail.com>
2020-05-23 23:00:54 -0300

    req bonus tentativa 1

--- script.js
+++ script.js
@@ -27,3 +27,22 @@ addMeme.addEventListener("change", function(){
   console.log(addMeme.files[0].name);
   myimg.src = URL.createObjectURL(addMeme.files[0]);
 });
+//------------------------------------------------
+let firebtn = document.querySelector("#fire");
+let waterbtn = document.querySelector("#water");
+let earthbtn = document.querySelector("#earth");
+let memeCont = document.querySelector("#meme-image-container")
+firebtn.addEventListener("click", function(){
+  memeCont.style.border = "3px dashed red";
+  
+});
+
+waterbtn.addEventListener("click", function(){
+  memeCont.style.border = "5px double blue";
+
+});
+
+earthbtn.addEventListener("click", function(){
+  memeCont.style.border = "6px groove green";
+
+});
