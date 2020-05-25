854fdf70c3d07286268ce623a69802f6b5df2016
Luiz Araujo <araujo.script@gmail.com>
2020-05-25 16:16:19 -0300

    atualização

--- script.js
+++ script.js
@@ -8,14 +8,17 @@ memeInsert.addEventListener("input", function() {
 memeImage.src = URL.createObjectURL(this.files[0]);
 })
 
+
 function inputText() {
 memeText.innerText = textInput.value;
 }
+
 
 textInput.addEventListener("keyup", inputText);
 memeInput.addEventListener("keyup", function() {
 memeText.innerText = inputText.value;
 })
+
 
 memeInput.addEventListener("input", function(){
 memeImage.src = URL.createObjectURL(this.files[0]);
