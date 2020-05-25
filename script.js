67c4d24cb58d5fddb6d8f234adbefc7dd6eafaf6
hleoc <informig@gmail.com>
2020-05-25 16:12:24 -0300

    Parte 1

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-window.onload = function{
+window.onload = function(){
+  
+    let botao1 = document.getElementById("fire");
+    let botao2 = document.getElementById("water");
+    let botao3 = document.getElementById("earth");
+    let caixaColorida = document.getElementById("meme-text");
+    let caixaTexto = document.getElementById("text-input");
 
+    botao1.addEventListener("click", mudaCor1);
+    botao2.addEventListener("click", mudaCor2);
+    botao3.addEventListener("click", mudaCor3);
 
+    function mudaCor1(){
+        caixaColorida.style.borderColor = "red";
+    }
+
+    function mudaCor2(){
+        caixaColorida.style.borderColor = "blue";
+    }
+
+    function mudaCor3(){
+        caixaColorida.style.borderColor = "green";
+    }
+
+    caixaTexto.addEventListener('keyup', incluiTexto)
     
+    function incluiTexto(){
+        caixaColorida.innerText = caixaTexto.value;
+    }
 }
 
 
-var p = document.createElement('p');
-p.innerHTML = 'Texto:';
-document.getElementById("meme-image-container").appendChild(p);
+
+
+
+
+
+
+
+
+//var p = document.createElement('p');
+//p.innerHTML = 'Texto:';
+//document.getElementById("meme-image-container").appendChild(p);
 
 
 
 
     
-document.querySelector("#text-input")
+//document.querySelector("#text-input")
