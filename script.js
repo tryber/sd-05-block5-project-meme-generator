5328b0375d6806f587a03f22d9a06131a16ce79f
SamuelPPlab <samuelpp.s@hotmail.com>
2020-05-25 15:56:06 -0300

    agora consegui colocar imagens

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ let botaoCriar = document.getElementById("criar");
 
 botaoCriar.addEventListener("click", function() {
     let texto = document.getElementById("text-input").value;
-    let textoimg = document.getElementById(id="meme-image-container");
+    let textoimg = document.getElementById("meme-text");
     textoimg.innerHTML = texto;
-})
+}) 
+
+  let loadFile = function(event) {
+    var output = document.getElementById('meme-image');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) // free memory
+    }
+  };
