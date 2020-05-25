dd939858b040b04ce32184b3218c621187fa3168
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-25 18:18:58 -0300

    Finishing

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let memeImage;
 let memeText;
 let textInput;
 let buttons;
+let examples;
 function process() {
   memeImage.src = URL.createObjectURL(this.files[0]);
 }
@@ -17,12 +18,21 @@ function initializeElements() {
     document.querySelector('#water'),
     document.querySelector('#earth'),
   ];
+  examples = [
+    document.querySelector('#meme-1'),
+    document.querySelector('#meme-2'),
+    document.querySelector('#meme-3'),
+    document.querySelector('#meme-4'),
+  ]
 }
 function setBorder(element) {
   const container = document.querySelector('#meme-image-container');
   const border = element.target.id;
   container.className = `image-container ${border}-border`;
   container.style.borderColor = element.target.style.backgroundColor;
+}
+function setMeme(element) {
+  memeImage.src = element.target.src;
 }
 window.onload = function () {
   initializeElements();
@@ -33,4 +43,7 @@ window.onload = function () {
   buttons.forEach((button) => {
     button.addEventListener('click', setBorder);
   });
+  examples.forEach((example) => {
+    example.addEventListener('click', setMeme);
+  })
 };
