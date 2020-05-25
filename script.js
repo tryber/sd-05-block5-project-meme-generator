2680fd5f335afc6779568ff1fc4c03cb4c64ad01
hleoc <informig@gmail.com>
2020-05-25 19:33:04 -0300

    Parte 1 correção 2

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-window.onload = function(){
-  
-    let botao1 = document.getElementById("fire");//botao muda cor 
-    let botao2 = document.getElementById("water");//botao muda cor
-    let botao3 = document.getElementById("earth");//botao muda cor
-    let caixaColorida = document.getElementById("meme-image-container");//elemento para servir de "container" para a imagem e para o texto do meme
-    let caixaTexto = document.getElementById("text-input");//caixa onde o texto é inserido 
-    let takePicture = document.getElementById("meme-insert");//botao para selecionar a imagem
-    let caixaTextoConteiner = document.getElementById("meme-text");
+window.onload = function (){
+    let botao1 = document.getElementById('fire');
+    let botao2 = document.getElementById('water');
+    let botao3 = document.getElementById('earth');
+    let caixaColorida = document.getElementById('meme-image-container');
+    let caixaTexto = document.getElementById('text-input');
+    let takePicture = document.getElementById('meme-insert');
+    let caixaTextoConteiner = document.getElementById('meme-text');
 
     botao1.addEventListener("click", mudaCor1);
     botao2.addEventListener("click", mudaCor2);
     botao3.addEventListener("click", mudaCor3);
 
     function mudaCor1(){
-        caixaColorida.style.borderColor = "red";
+        caixaColorida.style.borderColor = 'red';
     }
 
     function mudaCor2(){
-        caixaColorida.style.borderColor = "blue";
+        caixaColorida.style.borderColor = 'blue';
     }
 
     function mudaCor3(){
-        caixaColorida.style.borderColor = "green";
+        caixaColorida.style.borderColor = 'green';
     }
 
     caixaTexto.addEventListener('keyup', incluiTexto)
@@ -29,7 +28,4 @@ window.onload = function(){
     function incluiTexto(){
         caixaTextoConteiner.innerText = caixaTexto.value;
     }
-
-    takePicture.addEventListener('click', incluiImagem);
-    
 }
