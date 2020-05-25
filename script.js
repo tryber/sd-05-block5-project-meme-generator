42e82d3d4bfcd7658c7c8ace441c431811325e7a
Luiz Araujo <araujo.script@gmail.com>
2020-05-25 11:36:19 -0300

    script

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let text = document.getElementById("text-text")
+let textInput = document.getElementById("text-input");
+let memeText = document.getElementById("meme-text");
 let memeImageIn = document.getElementaryById("meme-insert")
 let memeImageOut = document.getElementById("meme-image")
+
+function inputText() {
+    memeText.innerText = textInput.value;
+}
+
+textInput.addEventListener("keyup", inputText);
 memeInput.addEventListener("keyup", function() {
-    memeText.Inner.HTML = inputText.valu;
+    memeText.innerText = inputText.value;
     })
-function createMeme() {
-    boxDoMeme.innerText = textInput.value;
-    }
+
+    memeInsert.addEventListener("input", function() {
+        memeImage.src = URL.createObjectURL(this.files[0]);
+      })
