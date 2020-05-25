1c3ac9ce7c6be39b087fefe8ecc5db1c9fdfa8fa
Luiz Araujo <araujo.script@gmail.com>
2020-05-25 01:12:59 -0300

    config css

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ let memeImage = document.getElementaryById("meme-text")
 function createMeme() {
     boxDoMeme.innerText = textInput.value;
     }
+    memeInput.addEventListener("Input, function()") {
+    memeImage.src=URL.createObjectURL(this.memeImage[0]);
+    }
