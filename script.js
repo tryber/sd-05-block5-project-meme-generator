fafee6256fd5c4135da51894d4fd9a3b3ece97cb
rfreitasbatista <rfreitasbatista@gmail.com>
2020-05-25 11:06:09 -0300

    Rumo aos 100%

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//TEXT INPUT
 let textInput = document.getElementById("text-input");
 let textOutput = document.getElementById("meme-text")
 textInput.addEventListener("keyup", memeTextAdd)
@@ -5,6 +6,8 @@ textInput.addEventListener("keyup", memeTextAdd)
 function memeTextAdd(){
 textOutput.innerHTML= textInput.value
 }
+
+//IMAGE INPUT
 let memeInput = document.getElementById("meme-insert")
 let memeImage = document.getElementById("meme-image")
 memeInput.addEventListener('change', function() {
@@ -20,6 +23,7 @@ function readURL(input) {
   }
 }
 
+//BORDER CHANGE BUTTONS
 let bdrChange = document.getElementById("meme-image-container")
 let fireBut = document.getElementById("fire");
 fireBut.addEventListener("click", function(){
@@ -38,3 +42,20 @@ earthBut.addEventListener("click", function(){
   bdrChange.style.border="6px groove green"
   console.log(bdrChange.style.border)
 })
+
+//MEME DEFAULT
+function selection (event) {
+  let target = event.target
+  console.log(target.src)
+  let mainImg = document.getElementById("meme-image")
+  mainImg.src = target.src
+}
+
+let meme01 = document.getElementById("meme-1")
+meme01.addEventListener("click", selection)
+let meme02 = document.getElementById("meme-2")
+meme02.addEventListener("click", selection)
+let meme03 = document.getElementById("meme-3")
+meme03.addEventListener("click", selection)
+let meme04 = document.getElementById("meme-4")
+meme04.addEventListener("click", selection)
