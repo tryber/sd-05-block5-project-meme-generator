f554d5c93c266184b0d96d22b6b9e0139f01f38b
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-05-25 19:24:16 -0300

    Fixing

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Deixando a caixa de texto interativa
 
-const caixaTexto = document.getElementById("text-input"); // Pegando o elemento(HTML) pelo Id. Local onde o usuário escreve
-const memeText = document.getElementById("meme-text"); // Local onde o texto deverá aparecer
+const caixaTexto = document.querySelector(".text-input"); // Pegando o elemento(HTML) pelo Id. Local onde o usuário escreve
+const memeText = document.querySelector(".meme-text"); // Local onde o texto deverá aparecer
 
 function inserirTexto() { // Definindo a função para fazer a interação do usuário com a caixa de texto
   memeText.innerText = caixaTexto.value; // dentro(innerHTML) do local escolhido, será inserido o valor (value) que o usuario escrever
@@ -10,8 +10,8 @@ caixaTexto.addEventListener('input', inserirTexto); // Atribuindo um evento para
 
 // Adicionando imagem para criação de novo meme
 
-const memeNovo = document.getElementById("meme-insert"); // Pegando o elemento(HTML) pelo Id. Local onde o usuário escolhe a imagem
-const caixaImagem = document.getElementById("meme-image"); // Local onde a imagem deverá aparecer
+const memeNovo = document.querySelector(".meme-insert"); // Pegando o elemento(HTML) pelo Id. Local onde o usuário escolhe a imagem
+const caixaImagem = document.querySelector(".meme-image"); // Local onde a imagem deverá aparecer
 
 function addNovoMeme() {
   caixaImagem.src = window.URL.createObjectURL(memeNovo.files[0]); // Colocando o local de onde a imagem será selecionada
