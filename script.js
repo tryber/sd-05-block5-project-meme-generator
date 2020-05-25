00290db240e496038488da7a844cd52e721456c7
hleoc <informig@gmail.com>
2020-05-25 12:53:16 -0300

    Estilizando a página 2

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function{
+
+
+    
+}
+
+
+var p = document.createElement('p');
+p.innerHTML = 'Texto:';
+document.getElementById("meme-image-container").appendChild(p);
+
+
+
+
+    
+document.querySelector("#text-input")
