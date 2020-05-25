a4caa4e8c3c06882f8c524e688429f9338db4e7d
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-25 17:30:07 -0300

    Improve codes

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-const fileInput = document.querySelector('#meme-insert');
-const memeImage = document.querySelector('#meme-image');
-const memeText = document.querySelector('#meme-text');
-const textInput = document.getElementById('text-input');
-
+let fileInput;
+let memeImage;
+let memeText;
+let textInput;
+let buttons;
+function process() {
+  memeImage.src = URL.createObjectURL(this.files[0]);
+}
+function initializeElements() {
+  fileInput = document.querySelector('#meme-insert');
+  memeImage = document.querySelector('#meme-image');
+  memeText = document.querySelector('#meme-text');
+  textInput = document.getElementById('text-input');
+  // Initialize Buttons
+  buttons = [
+    document.querySelector('#btn-fire'),
+    document.querySelector('#btn-water'),
+    document.querySelector('#btn-earth')
+  ];
+}
+function setBorder(element) {
+  const container = document.querySelector('#meme-image-container');
+  const border = element.target.id;
+  container.className = `image-container ${border.split('-')[1]}-border`;
+  console.log(element.target.style)
+  container.style.borderColor = element.target.style.backgroundColor;
+}
 window.onload = function () {
-
-  const utils = new Utils();
+  initializeElements();
   fileInput.addEventListener('change', process);
-
   textInput.addEventListener('keyup', function(el) {
     let txt = (el.target.value);
     memeText.innerText = txt;
   });
-
-  function process(element) {
-    utils.files = [];
-    utils.processFile(element);
-    loading();
-    function loading(){
-      if(utils.files.length > 0) {
-        memeImage.setAttribute('src', utils.files[0]);
-        return;
-      }
-      setTimeout(loading, 300);
-    }
-  }
+  buttons.forEach((button) => {
+    button.addEventListener("click", setBorder);
+  });
 }
