7ac7a848f5ff3ff049c244ea0078c2174bbf6f87
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-25 13:50:58 -0300

    adicionado função para alterar a border do container

--- script.js
+++ script.js
@@ -2,14 +2,28 @@ let memeImage = document.getElementById('meme-image');
 let memeInput = document.getElementById('meme-insert');
 let textInput = document.getElementById('text-input');
 let memeText = document.getElementById('meme-text');
+let fireButton = document.getElementById('fire');
+let waterButton = document.getElementById('water');
+let earthButton = document.getElementById('earth');
+let memeContainer = document.getElementById('meme-image-container');
 
+function fire(){
+  memeContainer.style.border = '3px dashed red'
+}
 
+function water(){
+  memeContainer.style.border = '5px double blue'
+}
 
-textInput.addEventListener('keyup', readText);
+function earth(){
+  memeContainer.style.border = '6px groove green'
+}
+
 
 function readText() {
   memeText.innerHTML = textInput.value;
 }
+
 
 memeInput.addEventListener('change', function() {
   readURL(this);
@@ -18,11 +32,16 @@ memeInput.addEventListener('change', function() {
 function readURL(input) {
   if(input.files && input.files[0]) {
     let reader = new FileReader();
-
+    
     reader.onload = function(event) {
       memeImage.setAttribute('src', event.target.result);
     }
-
+    
     reader.readAsDataURL(input.files[0]);
   }
 }
+
+fireButton.addEventListener('click', fire);
+waterButton.addEventListener('click', water);
+earthButton.addEventListener('click', earth);
+textInput.addEventListener('keyup', readText);
