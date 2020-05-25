18a774f31b6d0059ce4418e659918c9d163480b7
Larissa Palombo <larissampalombo@gmail.com>
2020-05-24 22:23:19 -0300

    ultimo commit para completar os requisitos

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
     const texto = document.getElementById('meme-text');
     const imagem = document.getElementById('meme-image');
     const textoEntrada = document.getElementById('text-input');
-    const imagemEntrada  = document.getElementById('meme-insert');
+    const imagemEntrada = document.getElementById('meme-insert');
 
     function printTexto (){
         texto.innerHTML = textoEntrada.value;
