2c393d7c9fe128b31f7ef56044379a4c19449774
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-26 15:15:56 -0300

    adicionado memes pré prontos

--- script.js
+++ script.js
@@ -6,6 +6,27 @@ let fogo = document.getElementById('fire');
 let agua = document.getElementById('water');
 let terra = document.getElementById('earth');
 let memeContainer = document.getElementById('meme-image-container');
+let meme1 = document.getElementById('meme-1');
+let meme2 = document.getElementById('meme-2');
+let meme3 = document.getElementById('meme-3');
+let meme4 = document.getElementById('meme-4');
+
+
+function presetImg() {
+  let target = event.target;
+  if (target === meme1) {
+    memeImage.src = "img/meme1.png";
+  }
+  if (target === meme2) {
+    memeImage.src = "img/meme2.png";
+  }
+  if (target === meme3) {
+    memeImage.src = "img/meme3.png";
+  }
+  if(target === meme4) {
+    memeImage.src = "img/meme4.png";
+  }
+}
 
 
 function trocaBorda() {
@@ -16,13 +37,11 @@ function trocaBorda() {
     memeContainer.style.borderStyle = 'dashed'; 
   }
   if (event.target === agua) {
-    // se o botao clicado foi o agua
     memeContainer.style.borderColor = 'blue'; 
     memeContainer.style.borderWidth = '5px'; 
     memeContainer.style.borderStyle = 'double'; 
   }
   if (event.target === terra) {
-    // se o botao clicado foi o terra
     memeContainer.style.borderColor = 'green'; 
     memeContainer.style.borderWidth = '6px'; 
     memeContainer.style.borderStyle = 'groove'; 
@@ -52,3 +71,7 @@ fogo.addEventListener('click', trocaBorda);
 agua.addEventListener('click', trocaBorda);
 terra.addEventListener('click', trocaBorda);
 textInput.addEventListener('keyup', readText);
+meme1.addEventListener('click', presetImg);
+meme2.addEventListener('click', presetImg);
+meme3.addEventListener('click', presetImg);
+meme4.addEventListener('click', presetImg);
