9b533fd883ffb11202a52b3c34667073fe852fd1
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-05-26 11:42:58 -0300

    Function textOnImg

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ let imgInsert = document.querySelector("#meme-insert");
 let container = document.querySelector("#meme-image-container");
 let image = document.querySelector("#meme-image");
 let text = document.querySelector("#meme-text");
+
+//Texto sobre imagem
+function textOnImg() {
+  text.innerHTML = textInput.value;
+}
+textInput.addEventListener("keyup", textOnImg);
