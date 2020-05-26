28c491bec04c648e433447afd31e75497ed797f5
Thiago <asm.thiago@gmail.com>
2020-05-26 17:30:53 -0300

    Será?

--- script.js
+++ script.js
@@ -5,20 +5,26 @@ textoInput.addEventListener("keyup", function(){
   textoMeme.innerHTML = textoInput.value
 });
 
+// const output = document.getElementById('meme-image');
+// document.getElementById('meme-insert').addEventListener('change', event => {
+//   output.src = '';
+//   //status.textContent = '';
+//   const file = event.target.files[0];
+//   if (!file.type.match('image.*')) {
+//     status.textContent = 'Error: The selected file does not appear to be an image.'
+//     return;
+//   }
+//   const reader = new FileReader();
+//   reader.addEventListener('load', event => {
+//     output.src = event.target.result;
+//     document.getElementById('meme-image-container').style.width='fit-content';
+//   });
+//   reader.readAsDataURL(file);
+//   console.log(file);
+// });
+
 const output = document.getElementById('meme-image');
-document.getElementById('meme-insert').addEventListener('change', event => {
-  output.src = '';
-  //status.textContent = '';
-  const file = event.target.files[0];
-  if (!file.type.match('image.*')) {
-    status.textContent = 'Error: The selected file does not appear to be an image.'
-    return;
-  }
-  const reader = new FileReader();
-  reader.addEventListener('load', event => {
-    output.src = event.target.result;
-    document.getElementById('meme-image-container').style.width='fit-content';
-  });
-  reader.readAsDataURL(file);
-  console.log(file);
+const imgInput = document.getElementById('meme-insert')
+imgInput.addEventListener('input', function () {
+  output.src = URL.createObjectURL(this.files[0]);
 });
