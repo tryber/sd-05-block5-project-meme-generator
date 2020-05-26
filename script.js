75f6d8b1a7e5b5a7e802ce4da974dde652685593
hleoc <informig@gmail.com>
2020-05-26 09:38:27 -0300

    Parte 1 correção 6

--- script.js
+++ script.js
@@ -1,33 +1,24 @@
-window.onload = function() {
-    const botao1 = document.getElementById('fire');
-    const botao2 = document.getElementById('water');
-    const botao3 = document.getElementById('earth');
-    const caixaColorida = document.getElementById('meme-image-container');
-    const caixaTexto = document.getElementById('text-input');
-    //const takePicture = document.getElementById('meme-insert');
-    const caixaTextoConteiner = document.getElementById('meme-text');
-    
-    function mudaCor1() {
-        caixaColorida.style.borderColor = 'red';
-    }
-
-    botao1.addEventListener('click', mudaCor1);
-    
-    function mudaCor2() {
-        caixaColorida.style.borderColor = 'blue';
-    }
-
-    botao2.addEventListener('click', mudaCor2);
-    
-    function mudaCor3() {
-        caixaColorida.style.borderColor = 'green';
-    }
-
-    botao3.addEventListener('click', mudaCor3);
-    
-    function incluiTexto() {
-        caixaTextoConteiner.innerText = caixaTexto.value;
-    }
-
-    caixaTexto.addEventListener('keyup', incluiTexto);
+window.onload = function () {
+  const botao1 = document.getElementById('fire');
+  const botao2 = document.getElementById('water');
+  const botao3 = document.getElementById('earth');
+  const caixaColorida = document.getElementById('meme-image-container');
+  const caixaTexto = document.getElementById('text-input');
+  const caixaTextoConteiner = document.getElementById('meme-text');
+  function mudaCor1 () {
+    caixaColorida.style.borderColor = 'red';
+  }
+  botao1.addEventListener('click', mudaCor1);
+  function mudaCor2 () {
+    caixaColorida.style.borderColor = 'blue';
+  }
+  botao2.addEventListener('click', mudaCor2);
+  function mudaCor3() {
+    caixaColorida.style.borderColor = 'green';
+  }
+  botao3.addEventListener('click', mudaCor3);
+  function incluiTexto() {
+    caixaTextoConteiner.innerText = caixaTexto.value;
+  }
+  caixaTexto.addEventListener('keyup', incluiTexto);
 };
