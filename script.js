856cd0b959e802fbf9ff701e44549017c042a3e6
luizf.avelar <luiz.avelar25@gmail.com>
2020-05-26 23:46:15 -0300

    Insert image

---
+++ script.js
@@ -0,0 +1,31 @@
+let fileTag = document.getElementById("meme-insert");
+let preview = document.getElementById("meme-image");
+    
+fileTag.addEventListener("change", function() {
+  changeImage(this);
+});
+
+function changeImage(input) {
+  let reader;
+
+  if (input.files && input.files[0]) {
+    reader = new FileReader();
+
+    reader.onload = function(e) {
+      preview.setAttribute('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+let textInsert = document.getElementById("text-input");
+let textShow = document.getElementById("meme-text");
+let confirmButton = document.getElementById("confirm-button");
+
+function memeText() {
+  textShow.innerHTML = textInsert.value;
+}
+
+confirmButton.addEventListener("click", memeText);
+
