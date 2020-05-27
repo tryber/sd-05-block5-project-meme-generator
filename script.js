a4c28d6fba848af889205734e4d3b8f57b0b139b
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-27 19:04:57 -0300

    add estilos css

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+// Função que adiciona texto digitado no meme
 function addMemeText() {
   const memeText = document.getElementById('text-input').value;
   document.querySelector('#meme-text').innerText = memeText;
 }
 
+// Evento do input do texto do meme
 const textInput = document.querySelector('#text-input');
 textInput.addEventListener('keyup', addMemeText);
 
+// Função que troca imagem do meme
 function changeImg(event) {
   if (event.target.files && event.target.files[0]) {
     const file = new FileReader();
@@ -18,3 +21,10 @@ function changeImg(event) {
 
 const memeInsert = document.querySelector('#meme-insert');
 memeInsert.addEventListener('change', changeImg, false);
+
+// Evento do botão fire
+const btnFire = document.querySelector('#fire');
+btnFire.addEventListener('click', function () {
+  const areaMeme = document.querySelector('#meme-image');
+  areaMeme.classList.add('fire');
+})
