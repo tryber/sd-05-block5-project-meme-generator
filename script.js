0ebce4bf408b436cce950e18b00ed106afa9cea9
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-05-27 16:14:08 -0300

    code climate adjusted

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-let imagem = document.querySelector("#meme-insert");
-let textBox = document.querySelector("#text-input");
-let div = document.querySelector("div");
-let text = document.querySelector('#meme-text');
-let img = document.querySelector('#meme-image')
+const imagem = document.querySelector('#meme-insert');
+const textBox = document.querySelector('#text-input');
+const div = document.querySelector('div');
+const text = document.querySelector('#meme-text');
+const img = document.querySelector('#meme-image');
 
-
-function insertImage () {
-    img.src = URL.createObjectURL(imagem.files[0]);
+function insertImage() {
+  img.src = URL.createObjectURL(imagem.files[0]);
 }
 function insertText() {
-    let insert = document.querySelector('#meme-text');
-    insert.style.position = "absolute";
-    insert.innerText = textBox.value;
-
+  let insert = document.querySelector('#meme-text');
+  insert.style.position = 'absolute';
+  insert.innerText = textBox.value;
 }   
 
 imagem.addEventListener('change', insertImage);
 textBox.addEventListener('keyup', insertText);
-
-
-
