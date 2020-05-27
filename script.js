07094eba4464d9edc56b826dc6e4d59981e3a6f5
luizf.avelar <luiz.avelar25@gmail.com>
2020-05-27 14:46:25 -0300

    Mandatory requirements

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-// let fileTag = document.getElementById("meme-insert");
-// let preview = document.getElementById("meme-image");
-    
-// fileTag.addEventListener("change", function() {
-//   changeImage(this);
-// });
-
-// function changeImage(input) {
-//   let reader;
-
-//   if (input.files) {
-//     reader = new FileReader();
-
-//     reader.onload = function(e) {
-//       preview.setAttribute("src", e.target.result);
-//       console.log(e.target.result);
-
-//     }
-//     reader.readAsDataURL(input.files[0]);
-//     console.log(input.files[0]);
-//   }
-// }
-
-// let textInput = document.getElementById("text-input"),
-//     memeText = document.getElementById("meme-text"),
-//     confirmButton = document.getElementById("confirm-button");
-
-// function insertText() {
-//   memeText.innerHTML = textInput.value;
-// }
-
-// confirmButton.addEventListener("click", insertText);
-/////////////////
-
 let textInput = document.getElementById("text-input"),
   textMeme = document.getElementById("meme-text"),
   memeInsert = document.getElementById("meme-insert"),
