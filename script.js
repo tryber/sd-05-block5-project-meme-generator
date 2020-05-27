e159009c7b81410f7c8410ce543b1d536c764fe8
luizf.avelar <luiz.avelar25@gmail.com>
2020-05-27 14:35:00 -0300

    .

--- script.js
+++ script.js
@@ -1,31 +1,51 @@
-let fileTag = document.getElementById("meme-insert");
-let preview = document.getElementById("meme-image");
+// let fileTag = document.getElementById("meme-insert");
+// let preview = document.getElementById("meme-image");
     
-fileTag.addEventListener("change", function() {
-  changeImage(this);
-});
+// fileTag.addEventListener("change", function() {
+//   changeImage(this);
+// });
 
-function changeImage(input) {
-  let reader;
+// function changeImage(input) {
+//   let reader;
 
-  if (input.files && input.files[0]) {
-    reader = new FileReader();
+//   if (input.files) {
+//     reader = new FileReader();
 
-    reader.onload = function(e) {
-      preview.setAttribute("src", e.target.result);
-    }
+//     reader.onload = function(e) {
+//       preview.setAttribute("src", e.target.result);
+//       console.log(e.target.result);
 
-    reader.readAsDataURL(input.files[0]);
-  }
+//     }
+//     reader.readAsDataURL(input.files[0]);
+//     console.log(input.files[0]);
+//   }
+// }
+
+// let textInput = document.getElementById("text-input"),
+//     memeText = document.getElementById("meme-text"),
+//     confirmButton = document.getElementById("confirm-button");
+
+// function insertText() {
+//   memeText.innerHTML = textInput.value;
+// }
+
+// confirmButton.addEventListener("click", insertText);
+/////////////////
+
+let textInput = document.getElementById("text-input"),
+  textMeme = document.getElementById("meme-text"),
+  memeInsert = document.getElementById("meme-insert"),
+  memeImage = document.getElementById("meme-image"),
+  confirmButton = document.getElementById("confirm-button");
+
+function insertText() {
+  textMeme.innerHTML = textInput.value;
 }
 
-let textInsert = document.getElementById("text-input"),
-    textShow = document.getElementById("meme-text"),
-    confirmButton = document.getElementById("confirm-button");
-
-function memeText() {
-  textShow.innerHTML = textInsert.value;
+function showImage(evento) {
+  let image = evento.target.files[0];
+  memeImage.src = URL.createObjectURL(image);
 }
 
-confirmButton.addEventListener("click", memeText);
-
+memeInsert.addEventListener("change", showImage);
+confirmButton.addEventListener("click", insertText);
