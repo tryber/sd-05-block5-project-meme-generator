55e4648b36ec5a67c6e005c92b19f075d092e942
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-05-28 21:25:09 -0300

    Revert to 9b533fd883ffb11202a52b3c34667073fe852fd1

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-//  Variáveis de entrada
-const textInput = document.querySelector('#text-input');
-const imgInsert = document.querySelector('#meme-insert');
-const image = document.querySelector('#meme-image');
-const text = document.querySelector('#meme-text');
+//Variáveis de entrada
+let textInput = document.querySelector("#text-input");
+let imgInsert = document.querySelector("#meme-insert");
+let container = document.querySelector("#meme-image-container");
+let image = document.querySelector("#meme-image");
+let text = document.querySelector("#meme-text");
 
-
-//  Texto sobre imagem
+//Texto sobre imagem
 function textOnImg() {
   text.innerHTML = textInput.value;
 }
-textInput.addEventListener('keyup', textOnImg);
-
-//  Inserindo imagem
-imgInsert.addEventListener('input', function () {
-  img.id = 'meme-image';
-  img.src = URL.createObjectURL(this.files[0]);
-});
+textInput.addEventListener("keyup", textOnImg);
