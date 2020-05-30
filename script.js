d924f02795f57d448081413aafe6b729bd230f6a
labelle <beelsdelonge@gmail.com>
2020-05-30 16:25:16 -0300

    arrumando codeclimate

--- script.js
+++ script.js
@@ -1,68 +1,63 @@
-//Para onde vai o texto e a imagem.
-let imgContainer = document.getElementById('meme-image-container');
-let textoInput = document.getElementById('meme-text');
-//Origem dos valores.
-let inputOrigem = document.getElementById('text-input');
-let sendImg = document.getElementById('meme-image');
+const imgContainer = document.getElementById('meme-image-container');
+const textoInput = document.getElementById('meme-text');
+const inputOrigem = document.getElementById('text-input');
+const sendImg = document.getElementById('meme-image');
 
 inputOrigem.addEventListener('keyup', function memeText() {
-    textoInput.innerHTML = inputOrigem.value;
+  textoInput.innerHTML = inputOrigem.value;
 });
 
-//Definindo as imagens selecionadas para a caixa de meme.
-let primeiroMeme = document.getElementById('meme-1');
+const primeiroMeme = document.getElementById('meme-1');
 primeiroMeme.addEventListener('click', function memeum() {
-    sendImg.src = primeiroMeme.src;
+  sendImg.src = primeiroMeme.src;
 });
 
-let segundoMeme = document.getElementById('meme-2');
+const segundoMeme = document.getElementById('meme-2');
 segundoMeme.addEventListener('click', function memedois() {
-    sendImg.src = segundoMeme.src;
+  sendImg.src = segundoMeme.src;
 });
 
-let terceiroMeme = document.getElementById('meme-3');
+const terceiroMeme = document.getElementById('meme-3');
 terceiroMeme.addEventListener('click', function memetres() {
-    sendImg.src = terceiroMeme.src;
+  sendImg.src = terceiroMeme.src;
 });
 
-let quartoMeme = document.getElementById('meme-4');
+const quartoMeme = document.getElementById('meme-4');
 quartoMeme.addEventListener('click', function memequatro() {
-    sendImg.src = quartoMeme.src;
+  sendImg.src = quartoMeme.src;
 });
 
-let quintoMeme = document.getElementById('meme-5');
+const quintoMeme = document.getElementById('meme-5');
 quintoMeme.addEventListener('click', function memecinco() {
-    sendImg.src = quintoMeme.src;
+  sendImg.src = quintoMeme.src;
 });
 
-let sextoMeme = document.getElementById('meme-6');
+const sextoMeme = document.getElementById('meme-6');
 sextoMeme.addEventListener('click', function memeseis() {
-    sendImg.src = sextoMeme.src;
+  sendImg.src = sextoMeme.src;
 });
 
-
-
-let bordaAgua = document.getElementById('water');
+const bordaAgua = document.getElementById('water');
 bordaAgua.addEventListener('click', function borda1() {
-    imgContainer.style.borderColor = "blue";
-    imgContainer.style.borderStyle = "double";
-    imgContainer.style.borderWidth = "5px";
+  imgContainer.style.borderColor = 'blue';
+  imgContainer.style.borderStyle = 'double';
+  imgContainer.style.borderWidth = '5px';
 });
 
-let bordaFogo = document.getElementById('fire');
+const bordaFogo = document.getElementById('fire');
 bordaFogo.addEventListener('click', function borda2() {
-    imgContainer.style.borderColor = "rgb(255, 0, 0)";
-    imgContainer.style.borderStyle = "dashed";
-    imgContainer.style.borderWidth = "3px";
+  imgContainer.style.borderColor = 'rgb(255, 0, 0)';
+  imgContainer.style.borderStyle = 'dashed';
+  imgContainer.style.borderWidth = '3px';
 });
 
-let bordaTerra = document.getElementById('earth');
+const bordaTerra = document.getElementById('earth');
 bordaTerra.addEventListener('click', function borda3() {
-    imgContainer.style.borderColor = "rgb(0, 128, 0)";
-    imgContainer.style.borderStyle = "groove";
-    imgContainer.style.borderWidth = "6px";
+  imgContainer.style.borderColor = 'rgb(0, 128, 0)';
+  imgContainer.style.borderStyle = 'groove';
+  imgContainer.style.borderWidth = '6px';
 });
 
 function setImage() {
-    sendImg.src = URL.createObjectURL(event.target.files[0]);
-};
+  sendImg.src = URL.createObjectURL(event.target.files[0]);
+}
