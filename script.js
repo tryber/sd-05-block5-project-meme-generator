ced025038a6d2ac952b1ca4049a7faec9b38f18d
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-05-31 15:43:38 -0300

    Code climate

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-//Variáveis de entrada
-let textInput = document.querySelector("#text-input");
-let imgInsert = document.querySelector("#meme-insert");
-let container = document.querySelector("#meme-image-container");
-let image = document.querySelector("#meme-image");
-let text = document.querySelector("#meme-text");
+// Variáveis de entrada
+const textInput = document.querySelector('#text-input');
+const imgInsert = document.querySelector('#meme-insert');
+const container = document.querySelector('#meme-image-container');
+const image = document.querySelector('#meme-image');
+const text = document.querySelector('#meme-text');
 
-//Texto sobre imagem
+// Texto sobre imagem
 function textOnImg() {
   text.innerHTML = textInput.value;
 }
-textInput.addEventListener("keyup", textOnImg);
+textInput.addEventListener('keyup', textOnImg);
