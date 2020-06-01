3a1eaea6b702e10028b9105010998261a7e397ab
Mau <mauricio.sdjr@gmail.com>
2020-06-01 11:28:52 -0300

    codeclimate

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
-//Meme Text
-let memeText = document.getElementById("meme-text");
-let textInput = document.getElementById("text-input");
+// Meme Text
+const memeText = document.getElementById('meme-text');
+const textInput = document.getElementById('text-input');
 
-textInput.addEventListener("keyup", function () {
+textInput.addEventListener('keyup', function () {
   memeText.innerHTML = textInput.value;
 });
 
-//Meme Image
-let memeImage = document.getElementById("meme-image");
-let memeInsert = document.getElementById("meme-insert");
+// Meme Image
+const memeImage = document.getElementById('meme-image');
+const memeInsert = document.getElementById('meme-insert');
 
-memeInsert.addEventListener("input", function () {
+memeInsert.addEventListener('input', function () {
   memeImage.src = URL.createObjectURL(this.files[0]);
 });
 
-//Border Buttons
-let memeImageContainer = document.getElementById("meme-image-container");
-let fire = document.getElementById("fire");
-let water = document.getElementById("water");
-let earth = document.getElementById("earth");
+// Border Buttons
+const memeImageContainer = document.getElementById('meme-image-container');
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
 
 function changeBorderStyle() {
   if (event.target === fire) {
-    memeImageContainer.style.border = "3px dashed red";
+    memeImageContainer.style.border = '3px dashed red';
   } else if (event.target === water) {
-    memeImageContainer.style.border = "5px double blue";
+    memeImageContainer.style.border = '5px double blue';
   } else if (event.target === earth) {
-    memeImageContainer.style.border = "6px groove green";
+    memeImageContainer.style.border = '6px groove green';
   }
 }
 
 function clickButton(button) {
-  button.addEventListener("click", changeBorderStyle);
+  button.addEventListener('click', changeBorderStyle);
 }
 
 clickButton(fire);
 clickButton(water);
 clickButton(earth);
 
-//Meme Image Click
-let meme1 = document.getElementById("meme-1");
-let meme2 = document.getElementById("meme-2");
-let meme3 = document.getElementById("meme-3");
-let meme4 = document.getElementById("meme-4");
+// Meme Image Click
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
 
 function clickImage(meme) {
-  meme.addEventListener("click", function () {
+  meme.addEventListener('click', function () {
     if (event.target === meme1) {
-      memeImage.src = "imgs/meme1.png";
+      memeImage.src = 'imgs/meme1.png';
     } else if (event.target === meme2) {
-      memeImage.src = "imgs/meme2.png";
+      memeImage.src = 'imgs/meme2.png';
     } else if (event.target === meme3) {
-      memeImage.src = "imgs/meme3.png";
+      memeImage.src = 'imgs/meme3.png';
     } else if (event.target === meme4) {
-      memeImage.src = "imgs/meme4.png";
+      memeImage.src = 'imgs/meme4.png';
     }
   });
 }
