9a15e8cf0188182b0aa88125293ffbf47bdaac50
Mau <mauricio.sdjr@gmail.com>
2020-06-01 11:13:05 -0300

    format document with prettier

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 //Meme Text
 let memeText = document.getElementById("meme-text");
 let textInput = document.getElementById("text-input");
-   
+
 textInput.addEventListener("keyup", function () {
   memeText.innerHTML = textInput.value;
 });
-   
+
 //Meme Image
 let memeImage = document.getElementById("meme-image");
 let memeInsert = document.getElementById("meme-insert");
-   
-memeInsert.addEventListener("input", function() {
+
+memeInsert.addEventListener("input", function () {
   memeImage.src = URL.createObjectURL(this.files[0]);
-  })
+});
 
 //Border Buttons
 let memeImageContainer = document.getElementById("meme-image-container");
@@ -21,18 +21,18 @@ let water = document.getElementById("water");
 let earth = document.getElementById("earth");
 
 function changeBorderStyle() {
-  if(event.target === fire) {
+  if (event.target === fire) {
     memeImageContainer.style.border = "3px dashed red";
-  } else if(event.target === water) {
+  } else if (event.target === water) {
     memeImageContainer.style.border = "5px double blue";
-  } else if(event.target === earth) {
-    memeImageContainer.style.border = "6px groove green"
+  } else if (event.target === earth) {
+    memeImageContainer.style.border = "6px groove green";
   }
-};
+}
 
 function clickButton(button) {
-  button.addEventListener("click", changeBorderStyle)
-};
+  button.addEventListener("click", changeBorderStyle);
+}
 
 clickButton(fire);
 clickButton(water);
@@ -44,18 +44,18 @@ let meme2 = document.getElementById("meme-2");
 let meme3 = document.getElementById("meme-3");
 let meme4 = document.getElementById("meme-4");
 
-function clickImage (meme) {
-  meme.addEventListener("click", function() {
-    if(event.target === meme1) {
-      memeImage.src="imgs/meme1.png";
-    } else if(event.target === meme2) {
-      memeImage.src="imgs/meme2.png";
-    } else if(event.target === meme3) {
-      memeImage.src="imgs/meme3.png"
-    } else if(event.target === meme4) {
-      memeImage.src="imgs/meme4.png"
+function clickImage(meme) {
+  meme.addEventListener("click", function () {
+    if (event.target === meme1) {
+      memeImage.src = "imgs/meme1.png";
+    } else if (event.target === meme2) {
+      memeImage.src = "imgs/meme2.png";
+    } else if (event.target === meme3) {
+      memeImage.src = "imgs/meme3.png";
+    } else if (event.target === meme4) {
+      memeImage.src = "imgs/meme4.png";
     }
-  })
+  });
 }
 
 clickImage(meme1);
