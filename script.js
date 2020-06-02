ac12f307100c323b7b740b164474fccb404bc938
Elisa <elisatcamillo@gmail.com>
2020-06-02 16:22:11 -0300

    image input js

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function text(){
   text_output.innerHTML = text_input.value;
 }
 
+let image_input = document.getElementById('meme-insert');
+let image_output = document.getElementById('meme-image')
+
+image_input.addEventListener('change', image);
+function image(event){
+  let read_img = new FileReader();
+  read_img.onload = function () {
+    let image_output = document.getElementById('meme-image');
+    image_output.src = read_img.result;
+  }
+  read_img.readAsDataURL(event.target.files[0]);
+}
