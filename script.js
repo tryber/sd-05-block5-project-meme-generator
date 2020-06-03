e18a170e89008edc07f4d4f5f5e166f8394bb562
Isabela Rosa <isabelarosa954@gmail.com>
2020-06-03 03:59:26 -0300

    corrigindo alguns erros codeclimate

--- script.js
+++ script.js
@@ -3,37 +3,34 @@ const textoEntrada = document.getElementById('text-input');
 const containerPrincipal = document.getElementById('meme-image-container');
 const textoMeme = document.getElementById('meme-text');
 const imgInserida = document.getElementById('meme-insert');
-const btnFire = document.getElementById('fire');
-const btnWater = document.getElementById('water');
-const btnEarth = document.getElementById('earth');
-const meme1 = document.getElementById('meme_1');
-const meme2 = document.getElementById('meme_2');
-const meme3 = document.getElementById('meme_3');
-const meme4 = document.getElementById('meme_4');
+//const btnFire = document.getElementById('fire');
+//const btnWater = document.getElementById('water');
+//const btnEarth = document.getElementById('earth');
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
 
-function textoDeEntrada() {
+/*function textoDeEntrada() {
   if (event.target === textoEntrada) {
     textoMeme.innerHTML = textoEntrada.value;
-}
-}
+  }
+}*/
 
 function selecionaBordas() {
-  if (event.target === fire)
-  {
+  if (event.target === fire) {
     containerPrincipal.style.borderColor = 'red';
     containerPrincipal.style.borderWidth = '3px';
     containerPrincipal.style.borderStyle = 'dashed';
   }
 
-  if (event.target === water)
-  {
+  if (event.target === water) {
     containerPrincipal.style.borderColor = 'blue';
     containerPrincipal.style.borderWidth = '5px';
     containerPrincipal.style.borderStyle = 'double';
   }
 
-  if (event.target === earth)
-  {
+  if (event.target === earth) {
     containerPrincipal.style.borderColor = 'green';
     containerPrincipal.style.borderWidth = '6px';
     containerPrincipal.style.borderStyle = 'groove';
@@ -41,40 +38,40 @@ function selecionaBordas() {
 }
 
 function selecionaMeme1() {
-  imgSelecionada.src = 'imgs/meme_1.png';
+  imgSelecionada.src = 'imgs/meme1.png';
 }
 
 function selecionaMeme2() {
-  imgSelecionada.src = 'imgs/meme_2.png';
+  imgSelecionada.src = 'imgs/meme2.png';
 }
 
 function selecionaMeme3() {
-  imgSelecionada.src = 'imgs/meme_3.png';
+  imgSelecionada.src = 'imgs/meme3.png';
 }
 
 function selecionaMeme4() {
-  imgSelecionada.src = 'imgs/meme_4.png';
+  imgSelecionada.src = 'imgs/meme4.png';
 }
 
 function imgClicada() {
-  if (event.target === meme_1)
+  if (event.target === meme1)
     selecionaMeme1();
 
-  if (event.target === meme_2)
+  if (event.target === meme2)
     selecionaMeme2();
 
-  if (event.target === meme_3)
+  if (event.target === meme3)
   selecionaMeme3();
 
-  if (event.target === meme_4)
-  selecionaMeme4();
+  if (event.target === meme4)
+    selecionaMeme4();
 }
 
-textoEntrada.addEventListener('keyup', function() {
+textoEntrada.addEventListener('keyup', function () {
   textoMeme.innerHTML = textoEntrada.value;
 });
 
-imgInserida.addEventListener('input', function() {
+imgInserida.addEventListener('input', function () {
   imgSelecionada.src = URL.createObjectURL(this.files[0]);
 });
 
