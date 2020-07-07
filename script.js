008942f7f8da137fbc2d58fbdb05af2c3e4dedff
Flavianny Maciel <flaviannymaciel@gmail.com>
2020-07-06 23:19:39 -0300

    vaiiii codeclimate

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Variáveis de entrada
-const textInputTop = document.querySelector("#text-input-top");
-const textInput = document.querySelector("#text-input");
-const memeText = document.getElementById("meme-text");
-const memeTextTop = document.getElementById("meme-text-top");
-const imgInsert = document.querySelector("#meme-insert");
-const image = document.querySelector("#meme-image");
-const memeOne = document.getElementById("meme-1");
-const memeTwo = document.getElementById("meme-2");
-const memeThree = document.getElementById("meme-3");
-const memeFour = document.getElementById("meme-4");
+const textInputTop = document.querySelector('#text-input-top');
+const textInput = document.querySelector('#text-input');
+const memeText = document.getElementById('meme-text');
+const memeTextTop = document.getElementById('meme-text-top');
+const imgInsert = document.querySelector('#meme-insert');
+const image = document.querySelector('#meme-image');
+const memeOne = document.getElementById('meme-1');
+const memeTwo = document.getElementById('meme-2');
+const memeThree = document.getElementById('meme-3');
+const memeFour = document.getElementById('meme-4');
 const fireButton = document.getElementById('fire');
 const waterButton = document.getElementById('water');
 const earthButton = document.getElementById('earth');
@@ -19,24 +19,24 @@ function textOnImg() {
   memeText.innerHTML = textInput.value;
   memeTextTop.innerHTML = textInputTop.value;
 }
-textInput.addEventListener("keyup", textOnImg);
-textInputTop.addEventListener("keyup", textOnImg);
+textInput.addEventListener('keyup', textOnImg);
+textInputTop.addEventListener('keyup', textOnImg);
 // Inserindo Imagem
-imgInsert.addEventListener("input", function () {
+imgInsert.addEventListener('input', function () {
   image.src = URL.createObjectURL(this.files[0]);
 });
 // Selecionando Memes
-memeOne.addEventListener("click", function () {
-  image.src = "meme1.jpeg";
+memeOne.addEventListener('click', function () {
+  image.src = 'meme1.jpeg';
 });
-memeTwo.addEventListener("click", function () {
-  image.src = "meme2.png";
+memeTwo.addEventListener('click', function () {
+  image.src = 'meme2.png';
 });
-memeThree.addEventListener("click", function () {
-  image.src = "meme3.png";
+memeThree.addEventListener('click', function () {
+  image.src = 'meme3.png';
 });
-memeFour.addEventListener("click", function () {
-  image.src = "meme4.png";
+memeFour.addEventListener('click', function () {
+  image.src = 'meme4.png';
 });
 // Selecionando bordas
 function changeBorderF() {
